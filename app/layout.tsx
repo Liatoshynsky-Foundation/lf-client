fab82f7f39d35d410b74f17890f27850d4b71fc8
bandvov <bandvov@gmail.com>
2025-05-04 13:45:49 +0300

    add header and footer.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from '@mui/material';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +28,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Container
+          maxWidth="md"
+          sx={{ border: '1px solid #ccc', padding: '20px' }}
+        >
+          <Header />
+          {children}
+          <Footer />
+        </Container>
       </body>
     </html>
   );
