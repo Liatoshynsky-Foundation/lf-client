1cf9d43d8f65fb37599987093592ecccfac416c4
Daniil Bokach <daniilbokach@gmail.com>
2025-05-21 15:25:12 +0300

    fix(config): fix problem with tests through jest.config

--- app/[lang]/page.test.tsx
+++ app/[lang]/page.test.tsx
@@ -1,15 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import Home from './page';
+
 describe('Home component', () => {
-  it('renders the Home component correctly', () => {
-    render(<Home />);
-    const homeElement = screen.getByText('Liatoshynsky project');
+  const params = Promise.resolve({ lang: 'en' });
+
+  it('renders the Home component correctly', async () => {
+    await act(async () => {
+      render(<Home params={params} />);
+    });
+
+    const homeElement = await screen.findByText(/Liatoshynsky project/i);
     expect(homeElement).toBeInTheDocument();
   });
 
-  it('renders the Home component with correct text', () => {
-    render(<Home />);
-    const homeElement = screen.getByText('Liatoshynsky project');
+  it('renders the Home component with correct text', async () => {
+    await act(async () => {
+      render(<Home params={params} />);
+    });
+
+    const homeElement = await screen.findByText(/Liatoshynsky project/i);
     expect(homeElement).toBeInTheDocument();
   });
 });
