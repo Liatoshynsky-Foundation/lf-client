73a84b9ec7c664e759d808a45aeb31ef2d098e98
Molly <132368159+qwqw-333@users.noreply.github.com>
2025-05-22 20:05:31 +0300

    chore: add GitFlow diagram and minor improvements (#73)
    * chore: add GitFlow diagram and minor improvements
    
    * chore(branchlint): update branch naming rules to match GitFlow docs
    
    ---------
    
    Co-authored-by: Molly <qwqw-333@proton.me>

--- .branchlintrc.config.js
+++ .branchlintrc.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  pattern: '^(feature|bugfix|hotfix|chore|release)/[0-9]+/[a-z0-9-]+$',
+  pattern:
+    '^(feature|bugfix|chore)/[0-9]+/[a-z0-9]+(-[a-z0-9]+)*$|(hotfix|release)/[0-9]+\\.[0-9]+\\.[0-9]+$',
 
   examples: [
-    'feature/1234/add-new-feature',
-    'bugfix/23/fix-login-issue',
-    'hotfix/1235/critical-security-fix',
-    'release/1236/prepare-release',
+    'feature/1234/add-login-ui',
+    'bugfix/23/fix-login-error',
+    'chore/987/update-dependencies',
+    'hotfix/1.0.1',
+    'release/1.2.3',
   ],
 
   exclude: ['main', 'develop'],
