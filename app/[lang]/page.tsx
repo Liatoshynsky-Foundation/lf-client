cf2ec6910360caa15e87367890cbab752402f6ab
Iarynovskyi <dimayar20061@gmail.com>
2025-05-24 00:39:35 +0300

    fix: Fix all lint errors

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,17 +1,7 @@
 'use client';
-import React, { ReactElement, use, useEffect } from 'react';
+import React, { ReactElement } from 'react';
 
-export default function Home({
-  params,
-}: {
-  params: Promise<{ lang: string }>;
-}): ReactElement {
-  const { lang } = use(params);
-
-  useEffect(() => {
-    console.log({ lang });
-  }, [lang]);
-
+export default function Home(): ReactElement {
   return (
     <>
       <h1>Liatoshynsky project</h1>
