dfa241f505a26ea69cabf4b122fd6881d5e6ea61
Iarynovskyi <dimayar20061@gmail.com>
2025-05-24 14:56:35 +0300

    fix: Delete MetaData in collaboration page

--- app/[lang]/collaboration/page.tsx
+++ app/[lang]/collaboration/page.tsx
@@ -1,16 +1,10 @@
-import { getTranslations, Locale } from '~/lib/i18n';
-import type { Metadata } from 'next';
+import {
+    getTranslations,
+    Locale
+} from '~/lib/i18n';
 
 interface CollaborationPageProps {
   params: Promise<{ lang: Locale }>;
-}
-
-export async function generateMetadata({ params }: CollaborationPageProps): Promise<Metadata> {
-  const { lang } = await params;
-  const { t } = await getTranslations(lang, 'collaborations');
-  return {
-    title: t('meta_data'),
-  };
 }
 
 export default async function CollaborationPage({ params }: CollaborationPageProps) {
