efda55c40eca9daf8aef1542a6c44f405fb711ff
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-05-26 20:47:32 +0300

    refactor: remove unnecessary style

--- app/components/dropdown-menu/DropdownMenu.style.ts
+++ app/components/dropdown-menu/DropdownMenu.style.ts
@@ -2,10 +2,8 @@ export const styles = {
   menu: {
     mt: 1,
     borderRadius: 2,
-     boxShadow: `
+    boxShadow: `
       0px 4px 8px rgba(0, 0, 0, 0.06),
-      0px 0px 4px rgba(0, 0, 0, 0.04)
-    `,
-    minWidth: 250,
+      0px 0px 4px rgba(0, 0, 0, 0.04) `,
   },
 };
