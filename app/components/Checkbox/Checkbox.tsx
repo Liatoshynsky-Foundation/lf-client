cfc21c72d42bc7e38157478b1f0768b11b048109
nika <vvvveronikaaaa666@gmail.com>
2025-05-26 19:29:09 +0300

    added onChange prop

--- app/components/Checkbox/Checkbox.tsx
+++ app/components/Checkbox/Checkbox.tsx
@@ -5,13 +5,12 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import { CheckboxProps } from '@mui/material/Checkbox';
 import { checkboxStyles } from './Checkbox.styles';
 
-
 interface CustomCheckboxProps extends CheckboxProps {
     label: React.ReactNode;
     size?: 'medium' | 'large';
 }
 type ReadonlyCustomCheckboxProps = Readonly<CustomCheckboxProps>;
-export default function CustomCheckbox({ disabled, defaultChecked, size, label }: ReadonlyCustomCheckboxProps) {
+export default function CustomCheckbox({ disabled, defaultChecked, size, label, onChange }: ReadonlyCustomCheckboxProps) {
     return (
         <Box>
             <FormGroup>
@@ -21,6 +20,7 @@ export default function CustomCheckbox({ disabled, defaultChecked, size, label }
                         size={size}
                         disabled={disabled}
                         defaultChecked={defaultChecked}
+                        onChange={onChange}
                     />}
                     label={label}
                 />
