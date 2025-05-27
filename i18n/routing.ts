d036eb8225d87ac5820b2e4025b35292131a29f9
Iarynovskyi <dimayar20061@gmail.com>
2025-05-27 15:50:53 +0300

    fix: Base uk not from browser and delete unused library

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,6 @@ import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en', 'uk'],
-  defaultLocale: 'uk'
+  defaultLocale: 'uk',
+  localeDetection: false
 });
