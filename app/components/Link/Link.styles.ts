44ff13f5b5eb6888d4fb81d95aa59661af9e0ed2
nika <vvvveronikaaaa666@gmail.com>
2025-05-27 18:03:48 +0300

    implemented link component, applied styles, covered with tests

---
+++ app/components/Link/Link.styles.ts
@@ -0,0 +1,14 @@
+export const linkStyles = {
+    color: '#52545A',
+    display: 'flex',
+    alignItems: 'center',
+    gap: 1,
+    textDecoration: 'none',
+    '&:hover': {
+        textDecoration: 'underline',
+        backgroundColor: 'transparent',
+    },
+    '&:active': {
+        backgroundColor: 'transparent',
+    }
+};
