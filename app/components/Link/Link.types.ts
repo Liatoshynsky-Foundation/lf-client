12bda97ba8bd42cf28021157e938107f2956a8b2
luvthenika <124041223+luvthenika@users.noreply.github.com>
2025-05-28 19:59:44 +0300

    Update Link.types.ts

--- app/components/Link/Link.types.ts
+++ app/components/Link/Link.types.ts
@@ -1,5 +1,3 @@
-// import { ReactElement } from 'react';
-// import { SvgIconProps } from '@mui/material/SvgIcon';
 import { StaticImageData } from 'next/image';
 type SVGType = StaticImageData | string | React.FC<React.SVGProps<SVGSVGElement>>;
 import { ButtonProps } from '@mui/material/Button'; 
