ceab734b2770194b254d68df7c0c11750043bfe7
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-05-29 13:15:59 +0300

    fix: get accustomed to new folder structure

--- app/shared/components/Footer/Footer.tsx
+++ app/shared/components/Footer/Footer.tsx
@@ -1,28 +1,28 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import FooterCopyrights from '~/components/Footer/FooterCopyrights/FooterCopyrights';
-import FooterContactInfo from '~/components/Footer/FooterContactInfo/FooterContactInfo';
+import FooterCopyrights from '~/shared/components/Footer/FooterCopyrights/FooterCopyrights';
+import FooterContactInfo from '~/shared/components/Footer/FooterContactInfo/FooterContactInfo';
 
 export const footerData = {
-  text: '© 2025 Liotoshynsky Foundation. Всі права захищені.',
-  links: [
-    { label: 'Політика конфіденційності', href: '/privacy' },
-    { label: 'Умови користування сайтом', href: '/terms' },
-    { label: 'Інформація для медіа / партнерів', href: '/media' },
-  ],
+    text: '© 2025 Liotoshynsky Foundation. Всі права захищені.',
+    links: [
+        { label: 'Політика конфіденційності', href: '/privacy' },
+        { label: 'Умови користування сайтом', href: '/terms' },
+        { label: 'Інформація для медіа / партнерів', href: '/media' },
+    ],
 };
 
 const contacts = {
-  title: 'ГРОМАДСЬКА ОРГАНІЗАЦІЯ \n«ФУНДАЦІЯ ЛЯТОШИНСЬКОГО»',
-  phone: '067 963 8366',
-  email: 'liatoshynsky@gmail.com',
+    title: 'ГРОМАДСЬКА ОРГАНІЗАЦІЯ \n«ФУНДАЦІЯ ЛЯТОШИНСЬКОГО»',
+    phone: '067 963 8366',
+    email: 'liatoshynsky@gmail.com',
 };
 
 export default function Footer() {
-  return (
-    <Box component="footer">
-      <FooterContactInfo contacts={contacts} />
-      <FooterCopyrights text={footerData.text} links={footerData.links} />
-    </Box>
-  );
+    return (
+        <Box component="footer">
+            <FooterContactInfo contacts={contacts} />
+            <FooterCopyrights text={footerData.text} links={footerData.links} />
+        </Box>
+    );
 }
