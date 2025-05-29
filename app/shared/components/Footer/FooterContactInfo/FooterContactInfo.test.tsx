4e86fd12983f5a42854e95dac1eed943bf6348c7
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-05-29 14:37:56 +0300

    fix: fix failing test

--- app/shared/components/Footer/FooterContactInfo/FooterContactInfo.test.tsx
+++ app/shared/components/Footer/FooterContactInfo/FooterContactInfo.test.tsx
@@ -4,84 +4,91 @@ import FooterContactInfo from './FooterContactInfo';
 import { useIsMobile } from '~/shared/hooks/is-mobile/useIsMobile';
 
 const contacts = {
-  title: 'Test Title',
-  phone: '123-456-7890',
-  email: 'test@example.com',
+    title: 'Test Title',
+    phone: '123-456-7890',
+    email: 'test@example.com',
 };
 
 jest.mock('~/shared/hooks/is-mobile/useIsMobile', () => ({
-  useIsMobile: jest.fn(),
+    useIsMobile: jest.fn(),
 }));
 
 describe('Contact information block inside of the Footer', () => {
-  beforeAll(() => {
-    (useIsMobile as jest.Mock).mockReturnValue(false);
+    describe('Contact information on desktop', () => {
+        beforeAll(() => {
+            (useIsMobile as jest.Mock).mockReturnValue(false);
 
-    Object.defineProperties(navigator, {
-      clipboard: {
-        value: {
-          writeText: jest.fn()
-        }
-      },
+            Object.defineProperties(navigator, {
+                clipboard: {
+                    value: {
+                        writeText: jest.fn()
+                    }
+                },
+            });
+
+            jest.spyOn(window, 'alert').mockImplementation(() => { });
+        });
+
+        afterAll(() => {
+            (useIsMobile as jest.Mock).mockRestore();
+        });
+
+        beforeEach(() => {
+            render(<FooterContactInfo contacts={contacts} />);
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('renders all contact information', () => {
+            expect(screen.getByText(contacts.title)).toBeInTheDocument();
+            expect(screen.getByText(contacts.phone)).toBeInTheDocument();
+            expect(screen.getByText(contacts.email)).toBeInTheDocument();
+        });
+
+        it('renders mailto email link with correct href', () => {
+            const emailLink = screen.getByRole('link', { name: contacts.email });
+            expect(emailLink).toHaveAttribute('href', `mailto:${contacts.email}`);
+        });
+
+        it('renders tel phone link with onClick handler', () => {
+            const phoneLink = screen.getByRole('link', { name: contacts.phone });
+            expect(phoneLink).toHaveAttribute('href', '#');
+
+            fireEvent.click(phoneLink);
+            expect(navigator.clipboard.writeText).toHaveBeenCalledWith(contacts.phone);
+            expect(navigator.clipboard.writeText).toHaveBeenCalledTimes(1);
+            expect(window.alert).toHaveBeenCalledWith('Номер телефону скопійовано до буферу обміну');
+        });
     });
-  });
 
-  afterAll(() => {
-    (useIsMobile as jest.Mock).mockRestore();
-  });
+    describe('Contact information on mobile', () => {
+        beforeAll(() => {
+            (useIsMobile as jest.Mock).mockReturnValue(true);
+        });
 
-  beforeEach(() => {
-    render(<FooterContactInfo contacts={contacts} />);
-  });
+        afterAll(() => {
+            (useIsMobile as jest.Mock).mockRestore();
+        });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+        beforeEach(() => {
+            render(<FooterContactInfo contacts={contacts} />);
+        });
 
-  it('renders all contact information', () => {
-    expect(screen.getByText(contacts.title)).toBeInTheDocument();
-    expect(screen.getByText(contacts.phone)).toBeInTheDocument();
-    expect(screen.getByText(contacts.email)).toBeInTheDocument();
-  });
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
 
-  it('renders mailto email link with correct href', () => {
-    const emailLink = screen.getByRole('link', { name: contacts.email });
-    expect(emailLink).toHaveAttribute('href', `mailto:${contacts.email}`);
-  });
+        it('renders mailto email link with correct href', () => {
+            const emailLink = screen.getByRole('link', { name: contacts.email });
+            expect(emailLink).toHaveAttribute('href', `mailto:${contacts.email}`);
+        });
 
-  it('renders tel phone link with onClick handler', () => {
-    const phoneLink = screen.getByRole('link', { name: contacts.phone });
+        it('renders tel phone link with href', () => {
+            const phoneLink = screen.getByRole('link', { name: contacts.phone });
+            expect(phoneLink).toHaveAttribute('href', `tel:${contacts.phone}`);
+        });
+    });
 
-    fireEvent.click(phoneLink);
-    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(contacts.phone);
-    expect(navigator.clipboard.writeText).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe('Contact information on mobile', () => {
-  beforeAll(() => {
-    (useIsMobile as jest.Mock).mockReturnValue(true);
-  });
-
-  afterAll(() => {
-    (useIsMobile as jest.Mock).mockRestore();
-  });
-
-  beforeEach(() => {
-    render(<FooterContactInfo contacts={contacts} />);
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('renders mailto email link with correct href', () => {
-    const emailLink = screen.getByRole('link', { name: contacts.email });
-    expect(emailLink).toHaveAttribute('href', `mailto:${contacts.email}`);
-  });
-
-  it('renders tel phone link with href', () => {
-    const phoneLink = screen.getByRole('link', { name: contacts.phone });
-    expect(phoneLink).toHaveAttribute('href', `tel:${contacts.phone}`);
-  });
-});
+})
--- app/shared/components/Footer/FooterContactInfo/FooterContactInfo.tsx
+++ app/shared/components/Footer/FooterContactInfo/FooterContactInfo.tsx
@@ -13,36 +13,36 @@ interface FooterContactInfoProps {
 }
 
 const FooterContactInfo: FC<FooterContactInfoProps> = ({ contacts }) => {
-  const isMobile = useIsMobile();
+    const isMobile = useIsMobile();
 
-  const copyPhoneToClipboard = () => {
-    navigator.clipboard.writeText(contacts.phone);
-    alert('Номер телефону скопійовано до буферу обміну');
-  };
+    const copyPhoneToClipboard = () => {
+        navigator.clipboard.writeText(contacts.phone);
+        alert('Номер телефону скопійовано до буферу обміну');
+    };
 
-  const telLinkProps = isMobile
-    ? { href: `tel:${contacts.phone}` }
-    : { onClick: copyPhoneToClipboard };
+    const telLinkProps = isMobile
+        ? { href: `tel:${contacts.phone}` }
+        : { onClick: copyPhoneToClipboard, href: '#' };
 
-  return (
-    <Box sx={styles.container}>
-      <Typography sx={styles.title}>{contacts.title}</Typography>
-      <Box>
-        <Box sx={styles.linkContainer}>
-          <Typography sx={styles.weakText}>Телефон: </Typography>
-          <Link sx={styles.link} {...telLinkProps}>
-            {contacts.phone}
-          </Link>
+    return (
+        <Box sx={styles.container}>
+            <Typography sx={styles.title}>{contacts.title}</Typography>
+            <Box>
+                <Box sx={styles.linkContainer}>
+                    <Typography sx={styles.weakText}>Телефон: </Typography>
+                    <Link sx={styles.link} {...telLinkProps}>
+                        {contacts.phone}
+                    </Link>
+                </Box>
+                <Box sx={styles.linkContainer}>
+                    <Typography sx={styles.weakText}>Email: </Typography>
+                    <Link sx={styles.link} href={`mailto:${contacts.email}`}>
+                        {contacts.email}
+                    </Link>
+                </Box>
+            </Box>
         </Box>
-        <Box sx={styles.linkContainer}>
-          <Typography sx={styles.weakText}>Email: </Typography>
-          <Link sx={styles.link} href={`mailto:${contacts.email}`}>
-            {contacts.email}
-          </Link>
-        </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default FooterContactInfo;
