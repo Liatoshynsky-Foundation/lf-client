884c4568cb2b4b2d42750bd3e426686626fadd28
Iarynovskyi <dimayar20061@gmail.com>
2025-05-29 21:22:53 +0300

    fix: prettier demonstration

--- app/shared/components/Header.test.ts
+++ app/shared/components/Header.test.ts
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
+
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 it('renders the Header component correctly', async () => {
-    render(Header());
-    const aboutElement = await screen.findByText(/Header/i);
-    expect(aboutElement).toBeInTheDocument();
+  render(Header());
+  const aboutElement = await screen.findByText(/Heade/i);
+  expect(aboutElement).toBeInTheDocument();
 });
