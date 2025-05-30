2a761f281a14fa6128c04b547fe3493075209aee
Iryna <30904237+irynalaitaruk@users.noreply.github.com>
2025-05-30 20:09:20 +0300

    Feature/55/tooltip component (#147)
    * Implemented tooltip component #55

---
+++ app/shared/components/design-system/all-components/tooltip/Tooltip.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TooltipCustom from './Tooltip';
+
+describe('TooltipCustom Component', () => {
+  test('renders the button with tooltip', () => {
+    render(<TooltipCustom showArrow={false} text="My Tooltip" />);
+    const buttonElement = screen.getByText(/My Tooltip/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test('renders tooltip with arrow when showArrow is true', async () => {
+    render(<TooltipCustom showArrow={true} text="My Tooltip with Arrow" />);
+    const buttonElement = screen.getByText(/My Tooltip with Arrow/i);
+    fireEvent.mouseOver(buttonElement);
+    const tooltipElement = await screen.findByText(/My Tooltip with Arrow/i);
+    expect(tooltipElement).toBeVisible();
+  });
+
+  test('renders tooltip without arrow when showArrow is false', async () => {
+    render(<TooltipCustom showArrow={false} text="My Tooltip" />);
+    const buttonElement = screen.getByText(/My Tooltip/i);
+    fireEvent.mouseOver(buttonElement);
+    const tooltipElement = await screen.findByText(/My Tooltip/i);
+    expect(tooltipElement).toBeVisible();
+  });
+});
