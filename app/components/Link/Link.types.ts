f1a576591d7b7e862f24e68f8bbc0fe7da71a094
nika <vvvveronikaaaa666@gmail.com>
2025-05-30 19:40:29 +0300

    removed redudant logic with svg

--- app/components/Link/Link.types.ts
+++ app/components/Link/Link.types.ts
@@ -1,11 +1,5 @@
-// import { ReactElement } from 'react';
-// import { SvgIconProps } from '@mui/material/SvgIcon';
-import { StaticImageData } from 'next/image';
-type SVGType = StaticImageData | string | React.FC<React.SVGProps<SVGSVGElement>>;
 import { ButtonProps } from '@mui/material/Button'; 
 export interface CustomLinkProps extends ButtonProps {
-  startSVG?: SVGType;
-  endSVG?: SVGType;
   children: React.ReactNode;
   path: string;
 }
