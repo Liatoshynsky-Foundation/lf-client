a2e1841da34d25d511ddd2c128d30d90295dbea2
nika <vvvveronikaaaa666@gmail.com>
2025-05-31 23:19:41 +0300

    fixing changes

---
+++ app/components/CustomLink/CustomLink.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import CustomLink from './CustomLink';
+import '@testing-library/jest-dom';
+import PlayIconSvg from '../../../public/icons/play-icon.svg';
+import { SvgImage } from '~/shared/components/svg-image/SvgImage';
+
+describe('CustomLink', () => {
+  test('should render CustomLink correct text and href', () => {
+    render(<CustomLink path="/test">Test Link</CustomLink>);
+
+    const link = screen.getByRole('link', { name: /test link/i });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', '/test');
+  });
+
+  test('should render front icon', () => {
+    render(
+      <CustomLink path="/" startIcon={<SvgImage src={PlayIconSvg} alt="" />}>
+        With Icon
+      </CustomLink>
+    );
+    expect(screen.getByText(/With Icon/i)).toBeInTheDocument();
+    expect(screen.getByTestId('front-icon')).toBeInTheDocument();
+  });
+
+  test('should render back icon (SVG image)', () => {
+    render(
+      <CustomLink path="/with-back-icon" endIcon={<SvgImage src={PlayIconSvg} alt="" />}>
+        With Back Icon
+      </CustomLink>
+    );
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src');
+  });
+});
