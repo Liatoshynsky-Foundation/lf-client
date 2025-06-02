81a03faf69e820393a11767438c71021faa5a7dd
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-06-02 21:55:53 +0300

    test: add tests logic+cov, created mock for dynamic function

--- app/shared/components/colored-svg/ColoredSvg.tsx
+++ app/shared/components/colored-svg/ColoredSvg.tsx
@@ -1,19 +1,36 @@
 import dynamic from 'next/dynamic';
 import { styles } from './ColoredSvg.styles';
 import { Box } from '@mui/material';
+import { validateSvgColor, validateSvgSize } from './ColoredSvg.validations';
 
 interface SvgProps {
   name: string;
   color: string;
-  width?: string | number;
-  height?: string | number;
+  width?: string;
+  height?: string;
 }
 
 export const Svg = ({ name, color, width, height }: SvgProps) => {
   const IconComponent = dynamic(() => import(`@public/${name}.svg`));
 
+  if (!validateSvgColor(color)) {
+    throw new Error(`Invalid color value: ${color}`);
+  }
+
+  if (width && height && !validateSvgSize(width, height)) {
+    throw new Error(`Invalid size values: width=${width}, height=${height}`);
+  }
+
+  const dynamicStyles = {
+    color: color,
+    '& svg': {
+      width: width ?? '24px',
+      height: height ?? '24px'
+    }
+  };
+
   return (
-    <Box sx={{ ...styles, color: color, '& svg': { width, height } }}>
+    <Box sx={{ ...styles, ...dynamicStyles }}>
       <IconComponent />
     </Box>
   );
