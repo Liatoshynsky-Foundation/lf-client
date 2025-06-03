850b9f8d73b1b390f4f1d093966841187d0e27a1
lizabre <lizabredichina@gmail.com>
2025-06-03 14:09:12 +0300

    test: updated tests

--- app/shared/components/Footer/footer-social-media/FooterSocialMedia.test.tsx
+++ app/shared/components/Footer/footer-social-media/FooterSocialMedia.test.tsx
@@ -3,13 +3,13 @@ import FooterSocialMedia from './FooterSocialMedia';
 import { render } from '@testing-library/react';
 
 describe('Social media icon buttons', () => {
+  const mockData = [
+    {
+      icon: SocialMediaTypes.Instagram,
+      href: 'https://www.instagram.com/'
+    }
+  ];
   it('should render an image based on the type', () => {
-    const mockData = [
-      {
-        icon: SocialMediaTypes.Instagram,
-        href: 'https://www.instagram.com/'
-      }
-    ];
     const expectedImage = 'instagram';
     render(<FooterSocialMedia media={mockData} />);
     const testImage = document.querySelector('img') as HTMLImageElement;
@@ -18,28 +18,28 @@ describe('Social media icon buttons', () => {
   });
 
   it('should render an image based on the type in text', () => {
-    const mockData = [
+    const updatedData = [
       {
-        icon: 'instagram',
-        href: 'https://www.instagram.com/'
+        ...mockData[0],
+        icon: 'instagram'
       }
     ];
     const expectedImage = 'instagram';
-    render(<FooterSocialMedia media={mockData} />);
+    render(<FooterSocialMedia media={updatedData} />);
     const testImage = document.querySelector('img') as HTMLImageElement;
     expect(testImage.alt).toContain(expectedImage);
     expect(testImage.src).toContain(`${expectedImage}.svg`);
   });
 
   it('should render a "shared" image if the type is for another social media', () => {
-    const mockData = [
+    const updatedData = [
       {
-        icon: 'mock text',
-        href: 'https://www.instagram.com/'
+        ...mockData[0],
+        icon: 'test'
       }
     ];
     const expectedImage = 'share.svg';
-    render(<FooterSocialMedia media={mockData} />);
+    render(<FooterSocialMedia media={updatedData} />);
     const testImage = document.querySelector('img') as HTMLImageElement;
     expect(testImage.src).toContain(expectedImage);
   });
