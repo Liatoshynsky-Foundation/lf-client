641c05421cfcb192fd8286132c7bc128a351d377
Iryna Khylchuk <khylchuk@icloud.com>
2025-06-04 16:49:48 +0300

    fix: fixed tests & added ref

--- app/shared/components/design-system/all-components/text-field/CustomTextField.tsx
+++ app/shared/components/design-system/all-components/text-field/CustomTextField.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { TextField, InputAdornment, SxProps } from '@mui/material';
+import { TextField, InputAdornment, SxProps, TextFieldProps as MuiTextFieldProps } from '@mui/material';
 import { outlinedStyles, standardStyles } from './CustomTextField.styles';
 import { SvgImage } from '~/shared/components/svg-image/SvgImage';
 
-type TextFieldProps = Readonly<{
+type CustomBaseProps = {
   variant?: 'standard' | 'outlined';
   disabled?: boolean;
   value?: string;
@@ -11,60 +11,53 @@ type TextFieldProps = Readonly<{
   startIcon?: string;
   endIcon?: string;
   placeholder?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   error?: boolean;
   sx?: SxProps;
-}>;
+};
 
-export default function CustomTextField({
-  variant = 'standard',
-  disabled,
-  value,
-  label,
-  startIcon,
-  endIcon,
-  placeholder,
-  onChange,
-  error,
-  sx,
-  ...props
-}: TextFieldProps) {
-  const variantStyles =
-    variant === 'outlined' ? outlinedStyles : standardStyles;
-  const effectiveStartIcon =
-    variant === 'standard' ? (startIcon ?? 'icons/search-icon.svg') : startIcon;
+type CustomTextFieldProps = CustomBaseProps & Omit<MuiTextFieldProps, keyof CustomBaseProps>;
 
-  return (
-    <TextField
-      id={label ?? 'custom-text-field'}
-      variant={variant}
-      disabled={disabled}
-      value={value}
-      label={label}
-      placeholder={placeholder}
-      onChange={onChange}
-      error={error}
-      sx={{ ...variantStyles, ...sx }}
-      slotProps={{
-        input: {
-          startAdornment: effectiveStartIcon && (
-            <InputAdornment position="start">
-              <SvgImage
-                src={effectiveStartIcon}
-                alt="start icon"
-                width={24}
-                height={24}
-              />
-            </InputAdornment>
-          ),
-          endAdornment: endIcon && (
-            <InputAdornment position="end">
-              <SvgImage src={endIcon} alt="end icon" width={24} height={24} />
-            </InputAdornment>
-          ),
-        },
-      }}
-      {...props}
-    />
-  );
-}
+const CustomTextField = React.forwardRef<HTMLInputElement, CustomTextFieldProps>(
+  (
+    { variant = 'standard', disabled, value, label, startIcon, endIcon, placeholder, onChange, error, sx, ...props },
+    ref
+  ) => {
+    const variantStyles = variant === 'outlined' ? outlinedStyles : standardStyles;
+    const effectiveStartIcon = variant === 'standard' ? (startIcon ?? 'icons/search-icon.svg') : startIcon;
+
+    return (
+      <TextField
+        id={label ?? 'custom-text-field'}
+        variant={variant}
+        disabled={disabled}
+        value={value}
+        label={label}
+        placeholder={placeholder}
+        onChange={onChange}
+        error={error}
+        sx={{ ...variantStyles, ...sx }}
+        inputRef={ref}
+        slotProps={{
+          input: {
+            startAdornment: effectiveStartIcon && (
+              <InputAdornment position="start">
+                <SvgImage src={effectiveStartIcon} alt="start icon" width={24} height={24} />
+              </InputAdornment>
+            ),
+            endAdornment: endIcon && (
+              <InputAdornment position="end">
+                <SvgImage src={endIcon} alt="end icon" width={24} height={24} />
+              </InputAdornment>
+            )
+          }
+        }}
+        {...props}
+      />
+    );
+  }
+);
+
+CustomTextField.displayName = 'CustomTextField';
+
+export default CustomTextField;
