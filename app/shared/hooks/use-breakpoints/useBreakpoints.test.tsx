72fcbc466799a599e14b80d7f7c85cfeb95f31a3
Vitaliy Kormylo <65959529+VKormylo@users.noreply.github.com>
2025-06-05 10:40:50 +0300

    feat: created useBreakpoints custom hook (#202)

---
+++ app/shared/hooks/use-breakpoints/useBreakpoints.test.tsx
@@ -0,0 +1,46 @@
+import { renderHook } from '@testing-library/react';
+import * as mediaQuery from '@mui/material/useMediaQuery';
+import * as styles from '@mui/material/styles';
+import useBreakpoints from './useBreakpoints';
+
+jest.mock('@mui/material/useMediaQuery', () => jest.fn());
+jest.mock('@mui/material/styles', () => {
+  const actual = jest.requireActual('@mui/material/styles');
+  return {
+    ...actual,
+    useTheme: jest.fn()
+  };
+});
+
+const mockUseMediaQuery = mediaQuery.default as jest.Mock;
+const mockUseTheme = styles.useTheme as jest.Mock;
+
+describe('useBreakpoints', () => {
+  beforeEach(() => {
+    mockUseTheme.mockReturnValue({
+      breakpoints: {
+        up: (key: string) => key,
+        between: (start: string, end: string) => `${start}-${end}`
+      }
+    });
+
+    mockUseMediaQuery
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(false)
+      .mockReturnValueOnce(false)
+      .mockReturnValueOnce(false);
+  });
+
+  it('should return correct breakpoints', () => {
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current).toEqual({
+      isDesktop: true,
+      isLaptopAndAbove: true,
+      isLaptop: false,
+      isTablet: false,
+      isMobile: false
+    });
+  });
+});
