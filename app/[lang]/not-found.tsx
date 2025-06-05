5183a010b1f30561afab6f0c3ce86220730dd9f4
Iarynovskyi Dmytro <164883382+Iarynovskyi@users.noreply.github.com>
2025-06-05 17:40:29 +0300

    fix: now after press 'go home' in not found page the local stay same (#208)

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,19 +1,23 @@
-import Link from 'next/link';
+import { Link } from '~/i18n/navigation';
 import { getTranslations } from 'next-intl/server';
 
 export default async function CustomNotFoundPage() {
   const t = await getTranslations('common');
 
-  return<>
-    <section style={{ marginBottom: '20px' }}>
-      {t('languageNotFound')}
-    </section>
-    <Link href='/' passHref style={{
-      color: 'yellow',
-      fontWeight: 'bold',
-      textDecoration: 'underline',
-    }}>
-      {t('goHome')}
-    </Link>
-  </>;
+  return (
+    <>
+      <section style={{ marginBottom: '20px' }}>{t('languageNotFound')}</section>
+      <Link
+        href="/"
+        passHref
+        style={{
+          color: 'yellow',
+          fontWeight: 'bold',
+          textDecoration: 'underline'
+        }}
+      >
+        {t('goHome')}
+      </Link>
+    </>
+  );
 }
