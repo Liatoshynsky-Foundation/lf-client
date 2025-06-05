595add5c90dd7fe433a87374c06607ef2682be90
lizabre <lizabredichina@gmail.com>
2025-06-05 13:41:09 +0300

    fix: some fixes

--- app/shared/components/Footer/footer-social-media/FooterSocialMedia.tsx
+++ app/shared/components/Footer/footer-social-media/FooterSocialMedia.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
-import { IconButton } from '../../design-system/all-components/icon-button/IconButton';
-import { SvgImage } from '../../svg-image/SvgImage';
 import { Box } from '@mui/material';
 import { styles } from './FooterSocialMedia.styles';
 import { SocialMediaTypes } from '~/types/enums/common.enums';
-
+import SocialMediaIcon from './social-media-icon/SocialMediaIcon';
+import { sanitizeSocialMediaType } from '~/lib/utils/sanitizeSocialMediaType';
 type LinkIcon = {
   icon: string | SocialMediaTypes;
   href: string;
@@ -14,41 +12,14 @@ interface FooterSocialMediaProps {
   media: LinkIcon[];
 }
 
-function sanitizeImageType(value: string): SocialMediaTypes {
-  value = value.toLocaleLowerCase();
-  if (Object.values(SocialMediaTypes).includes(value as SocialMediaTypes)) {
-    return value as SocialMediaTypes;
-  }
-  return SocialMediaTypes.AnotherMedia;
-}
-
 const FooterSocialMedia = ({ media }: FooterSocialMediaProps) => {
-  return (
-    <Box sx={styles.container}>
-      {media.map((item) => {
-        if (typeof item.icon === 'string') {
-          item.icon = sanitizeImageType(item.icon);
-        }
-        return (
-          <Link href={item.href} key={item.icon}>
-            <IconButton>
-              <div
-                style={{
-                  filter: 'brightness(0) invert(1)'
-                }}
-              >
-                <SvgImage
-                  src={item.icon === SocialMediaTypes.AnotherMedia ? '/icons/share.svg' : `/icons/${item.icon}.svg`}
-                  alt={item.icon}
-                  width={20}
-                  height={20}
-                />
-              </div>
-            </IconButton>
-          </Link>
-        );
-      })}
-    </Box>
-  );
+  const socialMedias = media.map((item) => {
+    if (typeof item.icon === 'string') {
+      item.icon = sanitizeSocialMediaType(item.icon);
+    }
+    return <SocialMediaIcon key={item.icon} icon={item.icon as SocialMediaTypes} href={item.href} />;
+  });
+
+  return <Box sx={styles.container}>{socialMedias}</Box>;
 };
 export default FooterSocialMedia;
