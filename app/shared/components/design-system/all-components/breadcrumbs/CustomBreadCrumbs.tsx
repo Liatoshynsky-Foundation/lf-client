a9d529b9355a4e575fab4ddd9f422e3bb3519754
nika <vvvveronikaaaa666@gmail.com>
2025-06-09 13:48:19 +0300

    fixed react import

--- app/shared/components/design-system/all-components/breadcrumbs/CustomBreadCrumbs.tsx
+++ app/shared/components/design-system/all-components/breadcrumbs/CustomBreadCrumbs.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import * as React from 'react';
+import React from 'react';
+
 
 import { BreadCrumbsStyles } from './BreadCrumbs.styles';
 import BreadcrumbsClient from './BreadCrumbsClient';
