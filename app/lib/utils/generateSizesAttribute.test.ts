6c362fb0736fe2c1995d183a9bfda3bfbd66cb81
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-06-09 20:27:34 +0300

    feat: created helper for generating sizes attribute for next/image

---
+++ app/lib/utils/generateSizesAttribute.test.ts
@@ -0,0 +1,21 @@
+import { generateSizesAttribute } from './generateSizesAttribute';
+
+describe('generateSizesAttribute', () => {
+  it('should return a correct sizes string based on breakpoints', () => {
+    const input = {
+      width: {
+        xs: 400,
+        sm: 600,
+        md: 800,
+        xl: 1000,
+        xxl: 816
+      }
+    };
+
+    const result = generateSizesAttribute(input);
+
+    expect(result).toBe(
+      '(max-width: 376px) 400px, (max-width: 768px) 600px, (max-width: 1024px) 800px, (max-width: 1448px) 1000px, 816px'
+    );
+  });
+});
---
+++ app/lib/utils/generateSizesAttribute.ts
@@ -0,0 +1,17 @@
+import { theme } from '~/ds-components/theme/Theme';
+import { ElementSizes } from '~/types/types/common.types';
+
+export function generateSizesAttribute(sizes: Pick<ElementSizes, 'width'>): string {
+  const breakpoints = theme.breakpoints.values;
+
+  const entries = Object.entries(sizes.width)
+    .filter(([bp]) => bp in breakpoints)
+    .sort((a, b) => breakpoints[a[0] as keyof typeof breakpoints] - breakpoints[b[0] as keyof typeof breakpoints])
+    .map(([bp, val], index, arr) => {
+      const max = breakpoints[bp as keyof typeof breakpoints];
+      const isLast = index === arr.length - 1;
+      return isLast ? `${val}px` : `(max-width: ${max}px) ${val}px`;
+    });
+
+  return entries.join(', ');
+}
