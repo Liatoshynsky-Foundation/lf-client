70e2941828e93e169e3a9f866993343921b5e7bf
luvthenika <124041223+luvthenika@users.noreply.github.com>
2025-06-09 16:07:17 +0300

    (SP: 3) Create Link element aligned with design system (#125)
    * implemented link component, applied styles, covered with tests
    
    * fixed sonar issues
    
    * improved styling, adjusted button props
    
    * Update Link component files
    
    * Update Link component files
    
    * removed comment
    
    * removed redudant logic with svg
    
    * removed redudant logic with svg
    
    * fixing changes
    
    * removinf files
    
    * moving files to shared
    
    * added should keyword instead of test keyword
    
    * removed duplicated files
    
    * fix redudant import
    
    * fix redudant import

---
+++ app/shared/components/design-system/all-components/link/CustomLink.tsx
@@ -0,0 +1,26 @@
+import { linkStyles } from './CustomLink.styles';
+import { NextLinkComposed } from './NextLink';
+import { Typography , Button , ButtonProps } from '@mui/material';
+
+export interface CustomLinkProps extends ButtonProps {
+  children: React.ReactNode;
+  path: string;
+}
+const CustomLink = ({ path, children, startIcon, endIcon }: CustomLinkProps) => {
+  return (
+    <Button
+      startIcon={startIcon}
+      endIcon={endIcon}
+      size="small"
+      sx={linkStyles.button}
+      component={NextLinkComposed}
+      disableElevation
+      disableRipple
+      to={{ pathname: path }}
+    >
+      <Typography sx={linkStyles.typography}>{children}</Typography>
+    </Button>
+  );
+};
+
+export default CustomLink;
