3bf82246597116ab2beb02cb4749abef69a55abf
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-06-10 15:23:30 +0300

    refactor: small refactor

--- app/lib/utils/generateSizesAttribute.ts
+++ app/lib/utils/generateSizesAttribute.ts
@@ -7,7 +7,7 @@ type BreakpointKey = keyof typeof theme.breakpoints.values;
 export function generateSizesAttribute(sizes: Pick<ElementSizes, 'width'>): string {
   const breakpoints = theme.breakpoints.values;
 
-  const entries = Object.entries(sizes.width || {})
+  const entries = Object.entries(sizes.width)
     .filter(([bp]) => bp in breakpoints)
     .sort((a, b) => breakpoints[a[0] as BreakpointKey] - breakpoints[b[0] as BreakpointKey])
     .map(([bp, val], index, arr) => {
