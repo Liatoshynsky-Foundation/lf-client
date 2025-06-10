0ba2b432345134229cd0fb07acf7f56cf5388b0a
Daniil Bokach <daniilbokach@gmail.com>
2025-06-10 18:51:30 +0300

    fix(ui): fix linter

--- app/shared/components/design-system/all-components/link/NextLink.tsx
+++ app/shared/components/design-system/all-components/link/NextLink.tsx
@@ -3,9 +3,7 @@ import MuiLink, { LinkProps as MuiLinkProps } from '@mui/material/Link';
 import clsx from 'clsx';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 import { useRouter } from 'next/router';
-
-import React from 'react';
-
+import * as React from 'react';
 
 interface NextLinkComposedProps
   extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>,
