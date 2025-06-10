d5c914a80c9e7adc0843313a441c4d5280ed36d1
lizabre <lizabredichina@gmail.com>
2025-06-10 15:51:57 +0300

    feat: start working on a block

---
+++ app/shared/components/main-page-sections/about-foundation/AboutFoundation.styles.ts
@@ -0,0 +1,9 @@
+export const styles = {
+  conatiner: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)'
+  },
+  col2: {
+    gridColumn: '2'
+  }
+};
---
+++ app/shared/components/main-page-sections/about-foundation/AboutFoundation.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material';
+
+import { styles } from './AboutFoundation.styles';
+
+const AboutFoundation = () => {
+  return (
+    <Box sx={styles.conatiner}>
+      <h1>ПРо ФундФціЮ</h1>
+      <Box sx={styles.col2}>
+        <h1>ПРо ФундФціЮ</h1>
+      </Box>
+    </Box>
+  );
+};
+export default AboutFoundation;
