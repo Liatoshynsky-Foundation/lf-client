e53bc7687ff2ea0a2c151043fdfd11d51b04530c
Iryna Khylchuk <khylchuk@icloud.com>
2025-06-11 12:43:12 +0300

    Implemented `FoundationFounders` section (#233)
    * feat: implemented foundation founders block
    
    * fix: fixed imports

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import LiatoshynskyOffice from '~/components/Liatoshynsky-office/LiatoshynskyOffice';
 
+import FoundationFounders from '~/shared/components/FoundationFounders/FoundationFounders';
+
 export default async function Home() {
-  return <LiatoshynskyOffice />;
+  return (
+    <>
+      <LiatoshynskyOffice />
+      <FoundationFounders />
+    </>
+  );
 }
--- app/shared/components/FoundationFounders/FoundationTeam/FoundationTeam.tsx
+++ app/shared/components/FoundationFounders/FoundationTeam/FoundationTeam.tsx
@@ -1,9 +1,13 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
-import PersonCard from '../design-system/all-components/person-card/PersonCard';
-import { SvgImage } from '../svg-image/SvgImage';
-import { styles } from './FoundationTeam.styles';
+import { styles } from '~/components/FoundationFounders/FoundationTeam/FoundationTeam.styles';
+import { SvgImage } from '~/components/svg-image/SvgImage';
+import PersonCard from '~/ds-components/person-card/PersonCard';
+
+interface FoundationTeamProps {
+  title: string;
+}
 
 interface Teammate {
   name: string;
@@ -40,24 +44,29 @@ const team: Teammate[] = [
   }
 ];
 
-const FoundationTeam = () => {
+const FoundationTeam: React.FC<FoundationTeamProps> = ({ title }) => {
   return (
     <Box sx={styles.container}>
-      {team.map((member, index) => (
-        <React.Fragment key={member.name}>
-          {index === 0 && (
-            <Box sx={styles.logo}>
-              <SvgImage src="/images/light-logo.svg" width={236} height={88} alt="logo" />
-            </Box>
-          )}
-          <PersonCard name={member.name} description={member.description} imgURL={member.photo} />
-          {(index + 1) % 2 === 0 && (
-            <Box sx={styles.logo}>
-              <SvgImage src="/images/light-logo.svg" width={236} height={88} alt="logo" />
-            </Box>
-          )}
-        </React.Fragment>
-      ))}
+      <Box sx={styles.titleWrapper}>
+        <Typography sx={styles.title}>{title}</Typography>
+      </Box>
+      <Box sx={styles.foundationTeam}>
+        {team.map((member, index) => (
+          <React.Fragment key={member.name}>
+            {index === 0 && (
+              <Box sx={styles.logo}>
+                <SvgImage src="/images/light-logo.svg" width={236} height={88} alt="logo" />
+              </Box>
+            )}
+            <PersonCard name={member.name} description={member.description} imgURL={member.photo} />
+            {(index + 1) % 2 === 0 && (
+              <Box sx={styles.logo}>
+                <SvgImage src="/images/light-logo.svg" width={236} height={88} alt="logo" />
+              </Box>
+            )}
+          </React.Fragment>
+        ))}
+      </Box>
     </Box>
   );
 };
