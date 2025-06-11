9661673ac4cc77bf5541fefad4058cd7eef2a613
lizabre <lizabredichina@gmail.com>
2025-06-11 16:56:13 +0300

    fix: some fixes

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 import FoundationFounders from '~/components/FoundationFounders/FoundationFounders';
 import LiatoshynskyOffice from '~/components/Liatoshynsky-office/LiatoshynskyOffice';
-import OurMission from '~/components/our-mission/OurMission';
 import AboutFoundation from '~/components/main-page-sections/about-foundation/AboutFoundation';
+import OurMission from '~/components/our-mission/OurMission';
 
 export default async function Home() {
   return (
     <>
       <AboutFoundation />
-<<<<<<< HEAD
       <OurMission />
-=======
->>>>>>> 5a87d16 (fix: updated from comments)
       <LiatoshynskyOffice />
       <FoundationFounders />
     </>
