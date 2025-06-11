1fe8a1caaa3c00f58c2969db17aa3637c8f31bba
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-06-11 13:09:21 +0300

    refactor: updated layout and added component to page.test.tsx, resolved comments

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import LiatoshynskyOffice from '~/components/Liatoshynsky-office/LiatoshynskyOffice';
 
 import FoundationFounders from '~/shared/components/FoundationFounders/FoundationFounders';
+import OurMission from '~/shared/components/our-mission/OurMission';
 
 export default async function Home() {
   return (
     <>
+      <OurMission />
       <LiatoshynskyOffice />
       <FoundationFounders />
     </>
