2cccd38b8c48e250978d81068a6dab494548d6f4
Iryna Khylchuk <khylchuk@icloud.com>
2025-06-18 21:00:00 +0300

    refactor: refactored SVG icon usage to improve compatibility with useClient
    Replaced dynamic import of SVG icons with direct component props passing.

--- app/shared/components/colored-svg/ColoredSvg.tsx
+++ app/shared/components/colored-svg/ColoredSvg.tsx
@@ -1,21 +1,18 @@
 import { Box } from '@mui/material';
-import dynamic from 'next/dynamic';
 import React from 'react';
 
 import { styles } from './ColoredSvg.styles';
 import { validateSvgColor, validateSvgSize } from './ColoredSvg.validations';
 
 interface SvgProps {
-  src: string;
+  Component: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   color: string;
   alt: string;
   width?: string;
   height?: string;
 }
 
-export const Svg = ({ src, color, alt, width, height }: SvgProps) => {
-  const IconComponent = dynamic(() => import(`~/public/${src}.svg`));
-
+export const Svg = ({ Component, color, alt, width, height }: SvgProps) => {
   if (!validateSvgColor(color)) {
     throw new Error(`Invalid color value: ${color}`);
   }
@@ -34,7 +31,7 @@ export const Svg = ({ src, color, alt, width, height }: SvgProps) => {
 
   return (
     <Box sx={{ ...styles, ...dynamicStyles }} role="img" aria-label={alt}>
-      <IconComponent />
+      <Component />
     </Box>
   );
 };
