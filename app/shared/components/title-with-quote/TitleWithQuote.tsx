9242f0f5a83e6e2a14f3274b82c245a4fc9193ff
Ulia <ulianaescha05@gmail.com>
2025-06-21 21:43:22 +0300

    feat: implement title with quote block

---
+++ app/shared/components/title-with-quote/TitleWithQuote.tsx
@@ -0,0 +1,38 @@
+import { Box, Typography } from '@mui/material';
+
+import QuoteBlock from '~/components/Quote/Quote';
+
+import { getStyles } from './TitleWithQuote.styles';
+import { SourceTextItem } from '~/types/types/quoteComponent';
+
+interface TitleWithQuoteProps {
+  title: string;
+  quoteText: string;
+  sourceText: SourceTextItem;
+  color: 'black' | 'brown';
+}
+
+const TitleWithQuote = ({ title, quoteText, sourceText, color }: TitleWithQuoteProps) => {
+  const styles = getStyles(color);
+  return (
+    <Box sx={styles.mainContainer}>
+      <Box sx={styles.titleSection}>
+        <Typography variant="h2" sx={styles.titleText}>
+          {title}
+        </Typography>
+      </Box>
+
+      <Box sx={styles.quoteSection}>
+        <QuoteBlock
+          quoteIconColor={'burgundy'}
+          mainTextColor={'burgundy'}
+          quoteText={quoteText}
+          sourceText={sourceText}
+          alignRight={false}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default TitleWithQuote;
