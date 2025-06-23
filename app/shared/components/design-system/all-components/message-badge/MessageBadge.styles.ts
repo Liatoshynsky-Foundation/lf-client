6526896788b2f166288ee2f7086be1f5d16952db
TARDeus524 <nevmermaks112@gmail.com>
2025-06-24 01:35:13 +0300

    feat: add message badge component and tests

---
+++ app/shared/components/design-system/all-components/message-badge/MessageBadge.styles.ts
@@ -0,0 +1,34 @@
+export const badgeStyles = {
+  '& .MuiBadge-dot': {
+    position: 'absolute',
+    top: 4,
+    right: 4
+  },
+  '& .MuiBadge-colorDefault': {
+    color: '#190D03'
+  },
+  '& .MuiBadge-colorPrimary': {
+    color: '#FCFCFC',
+    backgroundColor: '#5F0E0F'
+  },
+  '& .MuiBadge-colorSecondary': {
+    color: '#190D03',
+    backgroundColor: '#FCFCFC'
+  },
+  '& .MuiBadge-colorError': {
+    color: '#FCFCFC',
+    backgroundColor: '#E63C14'
+  },
+  '& img': {
+    position: 'absolute',
+    top: 11,
+    left: 11
+  },
+  width: 40,
+  height: 40
+};
+
+export const iconSizes = {
+  width: 18,
+  height: 18
+};
