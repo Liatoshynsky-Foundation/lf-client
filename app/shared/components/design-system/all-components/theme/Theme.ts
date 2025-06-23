b9668ce05aea69eb63e23a33cc2f0c2857ada2e8
nika <vvvveronikaaaa666@gmail.com>
2025-06-23 09:55:19 +0300

    move styles

--- app/shared/components/design-system/all-components/theme/Theme.ts
+++ app/shared/components/design-system/all-components/theme/Theme.ts
@@ -2,6 +2,7 @@ import { createTheme, PaletteColorOptions } from '@mui/material';
 import { Mulish, Oswald } from 'next/font/google';
 
 import {
+  hexCheckboxColors,
   mainHexPallete,
   rgbaMenuItemColors,
   rgbaSwitchColors,
@@ -687,6 +688,26 @@ export const theme = createTheme({
           },
           '&.Mui-selected': {
             backgroundColor: 'transparent'
+          }
+        }
+      }
+    },
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          color: hexCheckboxColors.iconColor,
+          '&.Mui-checked': {
+            color: hexCheckboxColors.iconCheckedBg
+          },
+          '&:hover': {
+            backgroundColor: hexCheckboxColors.hoverBg
+          },
+          '&.Mui-focusVisible': {
+            color: hexCheckboxColors.focusVisibleColor,
+            backgroundColor: hexCheckboxColors.focusVisibleBg
+          },
+          '&.Mui-disabled': {
+            color: hexCheckboxColors.disabledColor
           }
         }
       }
