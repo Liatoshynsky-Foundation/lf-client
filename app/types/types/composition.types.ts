7763c55392a17f1b77f1b5d0114ba1eb5daef9e4
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-06-25 13:54:48 +0300

    feat: create composition type

---
+++ app/types/types/composition.types.ts
@@ -0,0 +1,51 @@
+export type MusicSheet = {
+  url: string;
+  dateUploaded: string;
+  isFree: boolean;
+};
+
+export type AdditionalMenu = {
+  url: string;
+  key: string;
+  isAvailable: boolean;
+};
+
+export type Opus = {
+  id: string;
+  number: string;
+  title: {
+    uk: string;
+    en: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Genre = {
+  id: string;
+  key: string;
+  name: {
+    uk: string;
+    en: string;
+  };
+};
+
+export type Composition = {
+  id: string;
+  title: string;
+  year: number;
+  audioAvailable: boolean;
+  sheetAvailable: boolean;
+  songBlobUrl: string;
+  sheetMusic: MusicSheet[];
+  additionalMenu: {
+    listenComposition: AdditionalMenu;
+    viewYoutube: AdditionalMenu;
+    share: AdditionalMenu;
+    viewDetails: AdditionalMenu;
+  };
+  createdAt: string;
+  updatedAt: string;
+  opus: Opus;
+  genres: Genre[];
+};
