8937660e0d72e7db188b5dc52109687310e2c5c3
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-01 15:48:29 +0300

    fix: update test

--- app/shared/components/design-system/all-components/navigation-bar/dekstop-nav/DesktopNav.test.tsx
+++ app/shared/components/design-system/all-components/navigation-bar/dekstop-nav/DesktopNav.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import DesktopNav from './DesktopNav';
 import { ROUTES } from '~/constants/routes';
@@ -9,14 +8,25 @@ jest.mock('~/i18n/navigation', () => ({
 }));
 
 interface SvgImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  color: string;
   src: string;
   alt: string;
   width?: number;
   height?: number;
 }
 
-jest.mock('~/shared/components/svg-image/SvgImage', () => ({
-  SvgImage: (props: SvgImageProps) => <img {...props} alt="img" />
+jest.mock('~/shared/components/colored-svg/ColoredSvg.tsx', () => ({
+  Svg: (props: SvgImageProps) => <img {...props} alt="img" />
+}));
+
+jest.mock('~/public/icons/chevron-down.svg', () => ({
+  __esModule: true,
+  default: () => <svg data-testid="chevron-down-icon" />
+}));
+
+jest.mock('~/public/icons/chevron-up.svg', () => ({
+  __esModule: true,
+  default: () => <svg data-testid="chevron-up-icon" />
 }));
 
 class ResizeObserver {
@@ -42,20 +52,21 @@ describe('DesktopNav', () => {
     expect(screen.getByText('Співпраця')).toBeInTheDocument();
   });
 
-  it('should open and close dropdown menu on click', () => {
+  it('should openn and close dropdown menu on click', async () => {
     render(<DesktopNav />);
 
-    const dropdownButton = screen.getByText('Борис Лятошинський');
-    fireEvent.click(dropdownButton);
+    const dropdownTrigger = screen.getByText('Борис Лятошинський');
+    fireEvent.click(dropdownTrigger);
 
     expect(screen.getByText('Життєпис')).toBeInTheDocument();
     expect(screen.getByText('Творчість')).toBeInTheDocument();
     expect(screen.getByText('Дослідження та наукові роботи')).toBeInTheDocument();
 
-    const menuItem = screen.getByText('Життєпис');
-    fireEvent.click(menuItem);
+    fireEvent.click(screen.getByText('Життєпис'));
 
-    expect(screen.queryByText('Життєпис')).not.toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText('Життєпис')).not.toBeInTheDocument();
+    });
   });
 
   it('should have correct href attributes for buttons and dropdown items', () => {
