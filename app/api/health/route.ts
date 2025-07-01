bec10c413bef449a016d80e9201ea193f3ccf128
Iarynovskyi <dimayar20061@gmail.com>
2025-07-01 19:40:53 +0300

    feat: health check endpoint and delete zod validation for env

---
+++ app/api/health/route.ts
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+import { NextResponse } from 'next/server';
+
+import { errors } from '~/constants/errors';
+import { successResponse } from '~/utils/apiResponse';
+
+import { stateNames } from '~/constants';
+import dbConnect from '~/db/connect';
+
+const getMongooseConnectionState = (state: number): string => {
+  return stateNames[state] || 'UNKNOWN';
+};
+
+export async function GET() {
+  const healthDetails = {
+    status: 'DOWN',
+    timestamp: new Date().toISOString(),
+    dependencies: {
+      database: {
+        name: 'MongoDB (Mongoose)',
+        status: 'DOWN',
+        message: 'Health check not performed yet.',
+        details: {}
+      }
+    }
+  };
+
+  try {
+    await dbConnect();
+    const connection = mongoose.connection;
+    const db = connection.db;
+    const readyState = connection.readyState;
+    const connectionStatus = getMongooseConnectionState(readyState);
+    const isConnected = readyState === 1;
+
+    healthDetails.dependencies.database.details = {
+      connectionStatus: connectionStatus,
+      host: connection.host,
+      port: connection.port,
+      databaseName: connection.name
+    };
+
+    if (isConnected) {
+      if (!db) throw new Error(errors.FAILED_TO_CONNECT_DB);
+      const stats = await db.command({ dbStats: 1 });
+      const serverInfo = await db.admin().serverStatus();
+
+      healthDetails.dependencies.database.details = {
+        ...healthDetails.dependencies.database.details,
+        serverVersion: serverInfo.version,
+        collections: stats.collections,
+        objects: stats.objects,
+        dataSize: stats.dataSize,
+        storageSize: stats.storageSize,
+        uptime: serverInfo.uptime
+      };
+
+      healthDetails.status = 'UP';
+      healthDetails.dependencies.database.status = 'UP';
+      healthDetails.dependencies.database.message = '✅ Database is connected and healthy';
+
+      return successResponse(healthDetails);
+    } else {
+      healthDetails.dependencies.database.message = `Database connection state is: ${connectionStatus}`;
+      return NextResponse.json(healthDetails, { status: 503 });
+    }
+  } catch (e) {
+    healthDetails.dependencies.database.message = errors.FAILED_TO_CONNECT_DB;
+    healthDetails.dependencies.database.details = { e };
+    return NextResponse.json(healthDetails, { status: 503 });
+  }
+}
--- app/config/index.ts
+++ app/config/index.ts
@@ -1,7 +1,5 @@
-import { env } from '~/validators/env.schema';
-
 export const getMongoUrl = (): string => {
-  const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DB, MONGO_HOST, MONGO_PORT } = env;
+  const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DB, MONGO_HOST, MONGO_PORT } = process.env;
 
   if (MONGO_HOST === 'localhost') {
     return `mongodb://${MONGO_HOST}:${MONGO_PORT ?? 27017}/${MONGO_DB}`;
