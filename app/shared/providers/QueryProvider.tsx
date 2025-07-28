820e18753502b80e614e80de69d819f686b4b6ef
Mike-Popovych <volt251988@gmail.com>
2025-07-28 13:28:11 +0300

    feat: implement react query

---
+++ app/shared/providers/QueryProvider.tsx
@@ -0,0 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { type ReactNode } from 'react';
+
+import { queryClient } from '~/lib/queryClient';
+
+type QueryProviderProps = {
+  children: ReactNode;
+};
+
+const QueryProvider: React.FC<QueryProviderProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
