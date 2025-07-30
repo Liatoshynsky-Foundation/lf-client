5eae5ac7f8a2dbc8e0362ac988efa240ef3e737f
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-30 13:57:31 +0300

    fix: sonar issue

--- app/[lang]/terms/page.tsx
+++ app/[lang]/terms/page.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 import TermsIntro from '~/shared/components/blocks/terms-intro/TermsIntro';
 
 export default function Terms() {
-  return (
-    <>
-      <TermsIntro />
-    </>
-  );
+  return <TermsIntro />;
 }
