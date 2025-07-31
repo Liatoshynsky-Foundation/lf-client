204388f1e9a3234fdbed90626fa0b0a93a231f26
Roman Kryzhanivskyi <77616134+Kryzhanivsky@users.noreply.github.com>
2025-07-31 12:06:29 +0300

    fix: fix footer layout and responsiveness issues (#426)
    * fix: fix footer layout and responsiveness issues
    
    * fix: fix right-alignment and folding of navigation title rows

--- app/shared/components/Footer/FooterCopyrights/FooterCopyrights.styles.ts
+++ app/shared/components/Footer/FooterCopyrights/FooterCopyrights.styles.ts
@@ -4,30 +4,29 @@ export const styles = {
     maxWidth: '1300px',
     display: 'flex',
     flexDirection: 'column-reverse',
-    justifyContent: { xs: 'center', md: 'space-between' },
-    alignItems: { xs: 'center', md: 'flex-start' },
-    gap: '10px',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    gap: { xs: '32px', sm: '24px', lg: '8px' },
     fontFamily: 'Mulish, sans-serif'
   },
   text: {
-    fontSize: '16px',
-    color: '#5F0E0F',
+    fontSize: { xs: '14px', sm: '16px' },
+    color: '#412B21',
     fontWeight: 400,
     textAlign: { xs: 'left', md: 'left' },
-    maxWidth: '408px',
     width: '100%'
   },
   linkList: {
     display: 'flex',
-    alignItems: { xs: 'center', md: 'start' },
+    gap: { xs: '8px', md: 0 },
+    alignItems: 'start',
     justifyContent: { xs: 'flex-start', md: 'center' },
     listStyle: 'none',
     padding: 0,
     margin: 0,
-    maxWidth: '784px',
     flexWrap: { xs: 'wrap', lg: 'nowrap' },
     fontSize: '16px',
-    color: '#5F0E0F',
+    color: '#412B21',
     fontWeight: 400,
     width: { xs: '100%', md: 'auto' },
     marginLeft: '0',
@@ -40,24 +39,21 @@ export const styles = {
       '&:not(:first-of-type)::before': {
         content: '""',
         display: { xs: 'none', md: 'inline-block' },
-        width: '16px',
+        width: '2px',
         height: '16px',
         margin: '0 16px',
         verticalAlign: 'middle',
-        backgroundImage: 'url(/icons/bullet-icon.svg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain'
+        backgroundColor: '#574139'
       }
     }
   },
   link: {
     textDecoration: 'none',
-    color: '#5F0E0F',
     '&:hover': {
       textDecoration: 'underline',
       color: '#000'
     },
-    fontSize: { xs: '14px', md: '16px' },
+    fontSize: '16px',
     lineHeight: '150%'
   }
 };
