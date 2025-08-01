6c4e570422541a18a59c7907005eac20e211797a
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-08-01 13:32:20 +0300

    fix: sonar issue

--- app/lib/utils/getTokenFromHeader.ts
+++ app/lib/utils/getTokenFromHeader.ts
@@ -1,6 +1,6 @@
 export function getTokenFromHeader(request: { headers: Headers }) {
   const authHeader = request.headers.get('authorization');
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  if (!authHeader?.startsWith('Bearer ')) {
     return null;
   }
   return authHeader.slice('Bearer '.length);
