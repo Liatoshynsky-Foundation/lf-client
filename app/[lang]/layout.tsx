6a8221b40867d13bb078a0bdcfa3b6e230258b9b
nika <vvvveronikaaaa666@gmail.com>
2025-08-10 13:00:14 +0300

    deleted unused files

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -14,9 +14,7 @@ import ThemeProvider from '~/ds-components/theme/ThemeProvider';
 
 import { routing } from '~/i18n/routing';
 import { ColumnGuides } from '~/shared/components/column-guides/ColumnGuides';
-import CookieModalWrapper from '~/shared/components/cookie-modal/CookieModalWrapper';
 import EmotionProvider from '~/shared/components/emotion-provider/EmotionProvider';
-import { PanelProvider } from '~/shared/context/PanelContext';
 import QueryProvider from '~/shared/providers/QueryProvider';
 
 const geistSans = Geist({
@@ -66,55 +64,51 @@ export default async function RootLayout({ children, params }: RootLayoutParams)
         <meta name="emotion-insertion-point" content="" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${mulish.variable} ${oswald.variable}`}>
-        <PanelProvider>
-          <EmotionProvider>
-            <NextIntlClientProvider>
-              <ThemeProvider>
-                <QueryProvider>
-                  <Box sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
-                    <ColumnGuides />
-                    <Box
-                      sx={{
-                        padding: '20px 72px',
-                        height: '100vh',
-                        display: 'grid',
-                        gridTemplateColumns: 'repeat(12, 1fr)',
-                        columnGap: '40px',
-                        maxWidth: '1920px ',
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        width: '100%',
-                        flex: '1',
-                        [theme.breakpoints.down('md')]: {
-                          gridTemplateColumns: 'repeat(8, 1fr)',
-                          paddingLeft: '56px',
-                          paddingRight: '56px',
-                          columnGap: '20px'
-                        },
-                        [theme.breakpoints.down('sm')]: {
-                          gridTemplateColumns: 'repeat(4, 1fr)',
-                          paddingLeft: '24px',
-                          paddingRight: '24px',
-                          columnGap: '16px'
-                        }
-                      }}
-                    >
-                      <Box sx={{ gridColumn: '1 / -1' }}>
-                        <Header />
-                      </Box>
-                      <Box sx={{ display: 'grid', gridTemplateColumns: 'subgrid', gridColumn: '1 / -1' }}>
-                        {children}
-                      </Box>
-                      <Box sx={{ gridColumn: '1 / -1' }}>
-                        <Footer />
-                      </Box>
+        <EmotionProvider>
+          <NextIntlClientProvider>
+            <ThemeProvider>
+              <QueryProvider>
+                <Box sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
+                  <ColumnGuides />
+                  <Box
+                    sx={{
+                      padding: '20px 72px',
+                      height: '100vh',
+                      display: 'grid',
+                      gridTemplateColumns: 'repeat(12, 1fr)',
+                      columnGap: '40px',
+                      maxWidth: '1920px ',
+                      marginLeft: 'auto',
+                      marginRight: 'auto',
+                      width: '100%',
+                      flex: '1',
+                      [theme.breakpoints.down('md')]: {
+                        gridTemplateColumns: 'repeat(8, 1fr)',
+                        paddingLeft: '56px',
+                        paddingRight: '56px',
+                        columnGap: '20px'
+                      },
+                      [theme.breakpoints.down('sm')]: {
+                        gridTemplateColumns: 'repeat(4, 1fr)',
+                        paddingLeft: '24px',
+                        paddingRight: '24px',
+                        columnGap: '16px'
+                      }
+                    }}
+                  >
+                    <Box sx={{ gridColumn: '1 / -1' }}>
+                      <Header />
+                    </Box>
+                    <Box sx={{ display: 'grid', gridTemplateColumns: 'subgrid', gridColumn: '1 / -1' }}>{children}</Box>
+                    <Box sx={{ gridColumn: '1 / -1' }}>
+                      <Footer />
                     </Box>
                   </Box>
-                </QueryProvider>
-              </ThemeProvider>
-            </NextIntlClientProvider>
-          </EmotionProvider>
-        </PanelProvider>
+                </Box>
+              </QueryProvider>
+            </ThemeProvider>
+          </NextIntlClientProvider>
+        </EmotionProvider>
       </body>
     </html>
   );
