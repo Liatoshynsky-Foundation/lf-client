3dbc6d2035d4b02f10a2ad66e3b27ff02546e62c
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-08-13 23:47:59 +0300

    fix: fixed svg uses which were skewed by svg changes

--- app/[lang]/about-us/page.tsx
+++ app/[lang]/about-us/page.tsx
@@ -1,13 +1,5 @@
 import React, { ReactElement } from 'react';
 
-import Play from '~/public/icons/play.svg';
-import { Svg } from '~/shared/components/colored-svg/ColoredSvg';
-
 export default function AboutUs(): ReactElement {
-  return (
-    <div style={{ padding: '100px', backgroundColor: '#f0f0f0' }}>
-      AboutUs
-      <Svg Component={Play} alt="Description of the icon" color="#dbdafa" fill="#FF00ff" width="48px" height="48px" />
-    </div>
-  );
+  return <div>AboutUs</div>;
 }
