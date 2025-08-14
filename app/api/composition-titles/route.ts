8b0e2f666315d9728358105c92b886961b4134fb
nika <vvvveronikaaaa666@gmail.com>
2025-08-14 18:04:46 +0300

    changed method spelling

--- app/api/composition-titles/route.ts
+++ app/api/composition-titles/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from 'next/server';
 import { createRequestContainer } from '~/di/container';
 
 export async function GET() {
-  const titles = await createRequestContainer().resolve('artistryService').getAllTitles();
+  const titles = await createRequestContainer().resolve('artistryService').getAllCompositionTitles();
   return NextResponse.json(titles);
 }
