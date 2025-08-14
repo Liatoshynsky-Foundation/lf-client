4d473e5eb04f3d973c89a71275ac5201e4856d71
junarik <113181044+junarik@users.noreply.github.com>
2025-08-14 13:54:29 +0300

    style: change names of tests

--- app/shared/components/composition-search/LazyListItem.test.tsx
+++ app/shared/components/composition-search/LazyListItem.test.tsx
@@ -13,7 +13,7 @@ jest.mock('react-virtualized', () => {
 });
 
 describe('VirtualizedListbox', () => {
-  test('renders children inside virtualized list', () => {
+  test('should render children inside virtualized list', () => {
     render(
       <VirtualizedListbox>
         {[
@@ -31,7 +31,7 @@ describe('VirtualizedListbox', () => {
     expect(screen.getByText('Item 2')).toBeInTheDocument();
   });
 
-  test('forwards ref to container div', () => {
+  test('should forward ref to container div', () => {
     const ref = React.createRef<HTMLDivElement>();
 
     render(<VirtualizedListbox ref={ref}>{[<div key="1">Item</div>]}</VirtualizedListbox>);
