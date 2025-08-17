4bdf7302f7b1adee810017e6561cd5c8ac191567
nika <vvvveronikaaaa666@gmail.com>
2025-08-17 21:54:41 +0300

    updated test and remove console.log

--- app/services/composed/artistry-service/artistryService.ts
+++ app/services/composed/artistry-service/artistryService.ts
@@ -1,15 +1,7 @@
 import { Locale } from 'next-intl';
 
 import { ArtistryServiceDeps } from '~/domain/services/artistry.type';
-import {
-  createLocalizedCompositionsArraySchema,
-<<<<<<< HEAD
-  createLocalizedСompositionTitlesArraySchema
-=======
-  createLocalizedCompositionTitlesSchema,
-  createLocalizedCompositionTitlesSchemaArray
->>>>>>> f785588 (commiting before switching to anotherworking branch)
-} from '~/validators/artistry/composition.schema';
+import { createLocalizedCompositionsArraySchema } from '~/validators/artistry/composition.schema';
 import { createLocalizedGenresArraySchema } from '~/validators/artistry/genre.schema';
 
 export const createArtistryService = ({ compositionService }: ArtistryServiceDeps) => ({
@@ -20,7 +12,6 @@ export const createArtistryService = ({ compositionService }: ArtistryServiceDep
   },
   async getAllCompositions(locale: Locale, filter: string) {
     const allSongs = await compositionService.getAllCompositions(filter);
-<<<<<<< HEAD
     if (!allSongs) return [];
     return createLocalizedCompositionsArraySchema(locale).parse(allSongs);
   },
@@ -29,18 +20,6 @@ export const createArtistryService = ({ compositionService }: ArtistryServiceDep
     console.log(allTitles);
     if (!allTitles) return [];
 
-    return createLocalizedСompositionTitlesArraySchema(locale).parse(allTitles);
-=======
-    console.log(allSongs);
-    if (!allSongs) return [];
-    return createLocalizedCompositionsArraySchema(locale).parse(allSongs);
-  },
-  async getAllTitles(locale: Locale) {
-    const allTitles = await compositionService.getAllTitles();
-
-    if (!allTitles) return [];
-
-    return createLocalizedCompositionTitlesSchemaArray(locale).parse(allTitles);
->>>>>>> f785588 (commiting before switching to anotherworking branch)
+    return createLocalizedCompositionsArraySchema(locale).parse(allTitles);
   }
 });
