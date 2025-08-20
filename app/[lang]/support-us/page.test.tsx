251f6c82df7af4e096ff2a3f478e91ec730c094c
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-08-20 23:40:49 +0300

    refactor: last rename

--- app/[lang]/support-us/page.test.tsx
+++ app/[lang]/support-us/page.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import SupportUs from './page';
 
-jest.mock('~/components/blocks/faq/Faq', () => {
+jest.mock('~/components/blocks/FAQ/FAQ', () => {
   const MockFAQ = () => <div>FAQ</div>;
   MockFAQ.displayName = 'MockFAQ';
   return MockFAQ;
