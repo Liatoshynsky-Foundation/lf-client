ad90fefe95c9cb5001aeae6b39f46bf2f502aa19
Mike-Popovych <volt251988@gmail.com>
2025-08-29 10:46:43 +0300

    fix: resolve conflicts

--- app/services/composed/pages-data-service/pagesDataService.ts
+++ app/services/composed/pages-data-service/pagesDataService.ts
@@ -4,11 +4,13 @@ import { z } from 'zod';
 import { PageServiceDeps } from '~/domain/services/pagesService.type';
 import { createLocalizedAboutUsPageSchema } from '~/validators/pagesSchemas/pages/about-us.schema';
 import { createLocalizedPrivacyPolicyPageSchema } from '~/validators/pagesSchemas/pages/privacy-policy.schema';
+import { createLocalizedResearchPageSchema } from '~/validators/pagesSchemas/pages/research.schema';
 
 type AboutUsPage = z.infer<ReturnType<typeof createLocalizedAboutUsPageSchema>>;
 type PrivacyPolicyPage = z.infer<ReturnType<typeof createLocalizedPrivacyPolicyPageSchema>>;
+type ResearchPage = z.infer<ReturnType<typeof createLocalizedResearchPageSchema>>;
 
-type PageData = AboutUsPage | PrivacyPolicyPage;
+type PageData = AboutUsPage | PrivacyPolicyPage | ResearchPage;
 
 export const createPagesDataService = ({ pagesDataRepository }: PageServiceDeps) => ({
   async getPageData(slug: string, locale: Locale): Promise<PageData | null> {
@@ -17,7 +19,8 @@ export const createPagesDataService = ({ pagesDataRepository }: PageServiceDeps)
 
     const schemaMap = {
       'about-us': createLocalizedAboutUsPageSchema(locale),
-      'privacy-policy': createLocalizedPrivacyPolicyPageSchema(locale)
+      'privacy-policy': createLocalizedPrivacyPolicyPageSchema(locale),
+      research: createLocalizedResearchPageSchema(locale)
     } as const;
 
     const schema = schemaMap[slug as keyof typeof schemaMap];
