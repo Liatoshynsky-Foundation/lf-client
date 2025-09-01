3242b43a98cfa24bd60a2a119f425651bf8b26b9
Vitaliy Kormylo <65959529+VKormylo@users.noreply.github.com>
2025-09-01 16:14:18 +0300

    (SP: 0.5) Fixed issue with baseUrl in SEO on staging (#559)

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,11 @@
 import { MetadataRoute } from 'next';
+import { headers } from 'next/headers';
 
-export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : 'http://localhost:3000';
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const headersList = await headers();
+  const hostName = headersList.get('host') ?? 'localhost:3000';
+  const protocol = headersList.get('x-forwarded-proto') ?? 'http';
+  const baseUrl = `${protocol}://${hostName}`;
 
   return {
     rules: [
@@ -12,6 +16,6 @@ export default function robots(): MetadataRoute.Robots {
       }
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl
+    host: hostName
   };
 }
