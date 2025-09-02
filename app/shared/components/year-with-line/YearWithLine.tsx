3d3b3283d10d0119114525eeb540834e595ab978
Iarynovskyi Dmytro <164883382+Iarynovskyi@users.noreply.github.com>
2025-09-02 23:14:16 +0300

    feat: implement year with line component (#563)
    * feat: implement year with line component
    
    * fix: comment by ssasha

---
+++ app/shared/components/year-with-line/YearWithLine.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { Box } from '@mui/system';
+import { useEffect, useRef, useState } from 'react';
+
+import { theme } from '~/ds-components/theme/Theme';
+import useBreakpoints from '~/hooks/use-breakpoints/useBreakpoints';
+
+import { styles } from './YearWithLine.styles';
+const YearWithLine = ({ year }: { year: number }) => {
+  const Styles = styles(theme);
+
+  const numberStr = year.toString();
+  const [offset, setOffset] = useState(0);
+  const textRef = useRef<HTMLDivElement>(null);
+  const { isTablet, isMobile } = useBreakpoints();
+
+  useEffect(() => {
+    const calculateOffset = () => {
+      if (!textRef.current) return;
+
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      if (!ctx) return;
+
+      let fontSize = parseInt(theme.typography.customBold236.fontSize as string, 10);
+      if (isTablet) fontSize = parseInt(theme.typography.customBold132.fontSize as string, 10);
+      if (isMobile) fontSize = parseInt(theme.typography.customBold114.fontSize as string, 10);
+      ctx.font = `${theme.typography.customBold236.fontWeight} ${fontSize}px ${theme.typography.customBold236.fontFamily}`;
+
+      const text = numberStr.slice(0, 3);
+      const width = ctx.measureText(text).width;
+      setOffset(width);
+    };
+
+    calculateOffset();
+    window.addEventListener('resize', calculateOffset);
+    return () => {
+      window.removeEventListener('resize', calculateOffset);
+    };
+  }, [numberStr, isTablet, isMobile]);
+
+  return (
+    <Box sx={Styles.container}>
+      <Box ref={textRef} sx={Styles.yearBlock}>
+        <Box sx={Styles.line(offset)} />
+        <Box sx={Styles.year}>{numberStr}</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default YearWithLine;
