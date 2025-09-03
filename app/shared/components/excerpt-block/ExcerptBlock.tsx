8cef6463823353a6e0d0f72b162039edac3fc38e
ssashayurchenko <160735952+ssashayurchenko@users.noreply.github.com>
2025-09-03 23:06:42 +0300

    Add reusable quote component (#564)
    * feat: add reusable quote component
    
    * test: write test cases for ExcerptBlock component

---
+++ app/shared/components/excerpt-block/ExcerptBlock.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Box, Typography } from '@mui/material';
+
+import { SvgImage } from '../svg-image/SvgImage';
+import { styles } from './ExcerptBlock.styles';
+
+import useBreakpoints from '~/shared/hooks/use-breakpoints/useBreakpoints';
+
+interface ExcerptBlockProps {
+  quote: string;
+  source: string;
+}
+
+export default function ExcerptBlock({ quote, source }: ExcerptBlockProps) {
+  const { isMobile, isTablet } = useBreakpoints();
+
+  return isMobile ? (
+    <Box sx={styles.mobileView}>
+      <Typography sx={styles.quoteContainer} variant="customItalic18" fontWeight={700}>
+        {quote}
+      </Typography>
+      <Typography variant="customItalic14" fontWeight={500}>
+        {source}
+      </Typography>
+    </Box>
+  ) : (
+    <Box sx={styles.desktopView}>
+      <Box sx={styles.icon}>
+        <SvgImage src="/icons/quote.svg" alt="Quote icon" width={40} height={34} />
+      </Box>
+
+      <Typography sx={styles.sourceText(isTablet)} variant="customItalic14" fontWeight={500}>
+        {source}
+      </Typography>
+      <Typography variant="h5" fontStyle={'italic'} sx={styles.quoteText}>
+        {quote}
+      </Typography>
+    </Box>
+  );
+}
