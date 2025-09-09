285bc3dc0213f4a13bd2f723acb34b3f2ee224a0
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-09-09 21:57:35 +0300

    fix: fixed markup on support-us page

--- app/shared/components/blocks/support-foundation/SupportFoundation.styles.ts
+++ app/shared/components/blocks/support-foundation/SupportFoundation.styles.ts
@@ -1,19 +1,29 @@
 export const styles = {
   wrapper: {
+    display: 'grid',
+    gridTemplateColumns: 'subgrid',
+    gridColumn: '1 / -1',
     mt: '120px'
   },
 
   sectionTitle: {
-    mb: '34px'
+    mb: '34px',
+    gridColumn: '1 / -1'
   },
 
   donationSection: {
-    display: 'flex',
-    gap: '182px'
+    display: 'grid',
+    gridColumn: '1 / -1',
+    gridTemplateColumns: 'subgrid'
   },
 
   donationFormWrapper: {
-    pt: '43px'
+    pt: '43px',
+    gridColumn: '1 / 6'
+  },
+
+  infoSection: {
+    gridColumn: '7 / -1'
   },
 
   sectionSubtitle: {
