94f79a7416f9b4d6a7c3ac6e0faa3f5d808f1693
Oleksandra <ssasha.yurchenko@gmail.com>
2025-09-29 21:31:02 +0300

    refactor: pass icon as a prop

--- app/shared/components/design-system/all-components/card-with-text/CardWithText.const.ts
+++
@@ -1,6 +0,0 @@
-export const icons = [
-  '/icons/bullet-formats/icon-1.svg',
-  '/icons/bullet-formats/icon-2.svg',
-  '/icons/bullet-formats/icon-3.svg',
-  '/icons/bullet-formats/icon-4.svg'
-];
--- app/shared/components/design-system/all-components/card-with-text/CardWithText.tsx
+++ app/shared/components/design-system/all-components/card-with-text/CardWithText.tsx
@@ -1,23 +1,20 @@
 import { Box, Typography } from '@mui/material';
 
-import { icons } from './CardWithText.const';
 import { styles } from './CardWithText.styles';
 
 import PaperComponent from '~/shared/components/paper-component/PaperComponent';
 import { SvgImage } from '~/shared/components/svg-image/SvgImage';
 
 type CardWithTextProps = {
-  id: number;
+  icon?: string;
   title: string;
   list: Array<string>;
 };
 
-export default function CardWithText({ id, title, list }: Readonly<CardWithTextProps>) {
-  const icon = icons[id % icons.length];
-
+export default function CardWithText({ icon, title, list }: Readonly<CardWithTextProps>) {
   return (
     <PaperComponent sx={styles.container} childrenSx={styles.content}>
-      <SvgImage src={icon} alt="Bullet icon" width={48} height={48} />
+      {icon && <SvgImage src={icon} alt="Bullet icon" width={48} height={48} />}
       <Box sx={{ overflowY: 'auto' }}>
         <Typography sx={styles.title} variant="customBold20">
           {title}
