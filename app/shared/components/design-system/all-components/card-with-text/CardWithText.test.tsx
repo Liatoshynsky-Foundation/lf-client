ba2d87c3b80b58c4835297b650f7bc7bf66007d8
ssashayurchenko <160735952+ssashayurchenko@users.noreply.github.com>
2025-10-01 11:45:04 +0300

    Add card with the text component (#637)

---
+++ app/shared/components/design-system/all-components/card-with-text/CardWithText.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+
+import CardWithText from './CardWithText';
+
+describe('CardWithText', () => {
+  const smallList = ['Інтерв’ю та аналітика', 'Музична критика', 'Залучення медіа', 'Переклади матеріалів'];
+  const extendedList = [
+    'Звукове обладнання',
+    'Онлайн-трансляції',
+    'Логістика подій',
+    'Оцифрування архівів',
+    'Інтерв’ю та аналітика',
+    'Музична критика',
+    'Залучення медіа',
+    'Переклади матеріалів'
+  ];
+
+  it('should render the component with the necessary props', () => {
+    render(<CardWithText title="Some title" list={smallList} />);
+    expect(screen.getByText('Some title')).toBeInTheDocument();
+    for (const item of smallList) {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    }
+  });
+
+  it('should render the main icon if the prop icon is passed', () => {
+    render(<CardWithText title="Some title" list={smallList} icon="/icons/test-icon.svg" />);
+    expect(screen.getByAltText('Bullet icon')).toBeInTheDocument();
+  });
+
+  it('should not render the main icon if icon prop is not passed', () => {
+    render(<CardWithText title="Some title" list={smallList} />);
+    const mainIcon = screen.queryAllByAltText('Bullet icon');
+    expect(mainIcon.length).toBe(0);
+  });
+
+  it('should render all list items with correct number of list bullet icons', () => {
+    render(<CardWithText title="Some title" list={smallList} />);
+    const bullets = screen.getAllByAltText('List bullet icon');
+    expect(bullets.length).toBe(smallList.length);
+  });
+
+  it('should have vertical scroll container for extended list content', () => {
+    render(<CardWithText title="Some title" list={extendedList} />);
+
+    const titleElement = screen.getByText('Some title');
+
+    const scrollContainer = titleElement.parentElement;
+    expect(scrollContainer).toBeInTheDocument();
+
+    if (!scrollContainer) throw new Error('Scroll container not found');
+
+    const styles = globalThis.getComputedStyle(scrollContainer);
+    expect(styles.overflowY).toBe('auto');
+  });
+});
