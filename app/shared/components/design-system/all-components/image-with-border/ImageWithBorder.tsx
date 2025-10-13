1e4835ce46a5f3a742508d0e33a119ab18e65902
Oleksandra <ssasha.yurchenko@gmail.com>
2025-10-13 14:28:49 +0300

    refactor: make the borderWidth prop optional

--- app/shared/components/design-system/all-components/image-with-border/ImageWithBorder.tsx
+++ app/shared/components/design-system/all-components/image-with-border/ImageWithBorder.tsx
@@ -7,11 +7,17 @@ interface ImageWithBorderProps {
   image: string;
   width: number;
   height: number;
-  borderWidth: number;
+  borderWidth?: number;
   alt: string;
 }
 
-export default function ImageWithBorder({ image, width, height, borderWidth, alt }: Readonly<ImageWithBorderProps>) {
+export default function ImageWithBorder({
+  image,
+  width,
+  height,
+  borderWidth = 8,
+  alt
+}: Readonly<ImageWithBorderProps>) {
   return (
     <Box sx={styles.container(width, height)}>
       <Box sx={styles.border(borderWidth, height)} />
