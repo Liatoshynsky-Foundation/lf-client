813cea35ee2bd32013c3fcbc6655c411d64c7587
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-10-17 17:51:59 +0300

    feat: add page folder with unfinished metadata

--- app/domain/repositories/navigation.repository.ts
+++ app/domain/repositories/navigation.repository.ts
@@ -2,5 +2,5 @@ import type { NavigationDTO } from '../dto/navigation.dto';
 
 export type NavigationRepository = {
   getNavigation(): Promise<NavigationDTO[]>;
-  getSpecialNavigation(): Promise<NavigationDTO[]>;
+  getSpecialNavigation(): Promise<NavigationDTO | null>;
 };
