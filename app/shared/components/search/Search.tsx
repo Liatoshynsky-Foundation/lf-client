1a65eb27aca6cc2127a145a7300d680663be4cca
ruslansymonenko <symonenkors@gmail.com>
2025-10-21 18:08:28 +0300

    fix: replace useMemo with useCallback for renderOption function to fix lint warning

--- app/shared/components/search/Search.tsx
+++ app/shared/components/search/Search.tsx
@@ -130,7 +130,8 @@ export const Search = <T extends { title?: string | { en?: string; uk?: string }
       />
     );
   };
-  function renderOptionFn({ key, ...props }: object & { key: React.Key }, option: T): React.ReactNode {
+
+  const renderOption = useCallback(({ key, ...props }: object & { key: React.Key }, option: T): React.ReactNode => {
     return (
       <div {...props} key={key}>
         <ListItem disableGutters>
@@ -140,9 +141,8 @@ export const Search = <T extends { title?: string | { en?: string; uk?: string }
         </ListItem>
       </div>
     );
-  }
+  }, []);
 
-  const renderOption = useMemo(() => renderOptionFn, []);
   const getOptionLabel = (option: T) => {
     if (typeof option.title === 'string') {
       return option.title;
@@ -151,6 +151,7 @@ export const Search = <T extends { title?: string | { en?: string; uk?: string }
     }
     return '';
   };
+
   return (
     <Autocomplete<T, false, false, false>
       data-testid="music-search"
