a9b9548cee98dacd39d8816371820afef19e08d0
Roman Kryzhanivskyi <r.krijaniv@gmail.com>
2025-10-21 14:49:22 +0300

    feat: refactor the folder structure

--- app/[lang]/(pages)/media-about-us/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Typography } from '@mui/material';
-import React, { ReactElement } from 'react';
-
-export default function MediaAboutUs(): ReactElement {
-  return <Typography variant="h1">Media about us</Typography>;
-}
