3b6a2d3eb23dca1737820010fec4ed6048f0af10
Yaroslav Lischenko <yaroslav9732@gmail.com>
2025-10-22 01:41:21 +0300

    fix: remove any types in useFetchStaticFilters

--- app/shared/hooks/use-search/useFetchStaticFilters.ts
+++ app/shared/hooks/use-search/useFetchStaticFilters.ts
@@ -3,7 +3,7 @@ import { useLocale } from 'next-intl';
 import { tableClientService } from '~/services/client/tableService';
 import useQuery from '~/shared/hooks/query/useQuery';
 
-export type Selector<T> = (response: any) => T;
+export type Selector<TResponse, R = TResponse> = (response: TResponse) => R;
 
 export function useFetchStaticFilters<T = unknown>(endpoint: string | null) {
   const locale = useLocale();
@@ -12,8 +12,8 @@ export function useFetchStaticFilters<T = unknown>(endpoint: string | null) {
     queryKey: ['static-filters', endpoint ?? 'none', locale],
     queryFn: async (): Promise<T | null> => {
       if (!endpoint) return null;
-      const resp = await tableClientService.getTableStaticData<any>(endpoint, locale);
-      return resp as T;
+      const resp = await tableClientService.getTableStaticData<T>(endpoint, locale);
+      return resp;
     },
     options: {
       staleTime: Infinity
