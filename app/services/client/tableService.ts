56ef90b1dd7377166fed131528e917a19e4d17b5
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-10-22 18:49:57 +0300

    fix(types): replace explicit any with tableParams(#711)

--- app/services/client/tableService.ts
+++ app/services/client/tableService.ts
@@ -1,5 +1,7 @@
 import { Locale } from 'next-intl';
 
+import type { tableParams } from '~/types/types/tableParams.types';
+
 import { getFullUrl } from '~/lib/utils/getFullUrl';
 import { baseService } from '~/services/client/baseService';
 
@@ -14,7 +16,7 @@ async function getTableStaticData<T>(pathname: string, locale: Locale): Promise<
   });
 }
 
-async function getTableData<T>(pathname: string, locale: Locale, params: Record<string, any>): Promise<T[]> {
+async function getTableData<T>(pathname: string, locale: Locale, params: tableParams): Promise<T[]> {
   const url = getFullUrl({
     pathname,
     searchParameters: {
