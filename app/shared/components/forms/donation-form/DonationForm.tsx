32550a65c0f267914f8ef4fa695c3a990e768e5e
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-10-23 20:34:40 +0300

    feat: update donation presets switch from additive to selectable(#747)

--- app/shared/components/forms/donation-form/DonationForm.tsx
+++ app/shared/components/forms/donation-form/DonationForm.tsx
@@ -108,12 +108,11 @@ function DonationForm() {
       id={item.toString()}
       variant="outlined"
       onClick={() => {
-        const newSum = (donationSum || 0) + item;
-        setDonationSum(newSum);
-        if (touched) setHasError(newSum === 0);
+        setDonationSum(item);
+        if (touched) setHasError(donationSum === 0);
       }}
     >
-      <Typography variant="customSemiBold18">+{item}</Typography>
+      <Typography variant="customSemiBold18">{item}</Typography>
       <Typography variant="customMedium16" sx={style.currencySuggestion}>
         {currency}
       </Typography>
