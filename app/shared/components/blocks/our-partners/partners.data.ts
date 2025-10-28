410a8496a9f14c25b75e2320e005edc5fd3bca48
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-28 17:26:26 +0200

    refactor: simplify partnersMock data structure

--- app/shared/components/blocks/our-partners/partners.data.ts
+++ app/shared/components/blocks/our-partners/partners.data.ts
@@ -5,59 +5,20 @@ export interface Partner {
   name: string;
 }
 
-export const partnersMock: Partner[] = [
-  {
-    id: 'musicHouse',
-    link: '/',
-    img: '/images/partners/national-music-house.png',
-    name: 'Національний будинок музики'
-  },
-  {
-    id: 'philharmonic',
-    link: '/',
-    img: '/images/partners/national-philharmonic.png',
-    name: 'Національна філармонія України'
-  },
-  {
-    id: 'liatoshynsky',
-    link: '/',
-    img: '/images/partners/liatoshynsky-space.png',
-    name: 'Liatoshynsky Space'
-  },
-  {
-    id: 'masterKlass',
-    link: '/',
-    img: '/images/partners/masterklass.png',
-    name: 'Master Klass'
-  },
-  {
-    id: 'kolomyia',
-    link: '/',
-    img: '/images/partners/kolomyia-philharmonic.png',
-    name: 'Коломийська філармонія'
-  },
-  {
-    id: 'axon',
-    link: '/',
-    img: '/images/partners/axon-partners.png',
-    name: 'Axon Partners'
-  },
-  {
-    id: 'espreso',
-    link: '/',
-    img: '/images/partners/espreso.png',
-    name: 'Еспресо'
-  },
-  {
-    id: 'softserve',
-    link: '/',
-    img: '/images/partners/softserve.png',
-    name: 'SoftServe Academy'
-  },
-  {
-    id: 'opentech',
-    link: '/',
-    img: '/images/partners/opentech.png',
-    name: 'OpenTech'
-  }
+const partnerData: Omit<Partner, 'link'>[] = [
+  { id: 'musicHouse', name: 'Національний будинок музики', img: 'national-music-house.png' },
+  { id: 'philharmonic', name: 'Національна філармонія України', img: 'national-philharmonic.png' },
+  { id: 'liatoshynsky', name: 'Liatoshynsky Space', img: 'liatoshynsky-space.png' },
+  { id: 'masterKlass', name: 'Master Klass', img: 'masterklass.png' },
+  { id: 'kolomyia', name: 'Коломийська філармонія', img: 'kolomyia-philharmonic.png' },
+  { id: 'axon', name: 'Axon Partners', img: 'axon-partners.png' },
+  { id: 'espreso', name: 'Еспресо', img: 'espreso.png' },
+  { id: 'softserve', name: 'SoftServe Academy', img: 'softserve.png' },
+  { id: 'opentech', name: 'OpenTech', img: 'opentech.png' }
 ];
+
+export const partnersMock: Partner[] = partnerData.map((p) => ({
+  ...p,
+  link: '/',
+  img: `/images/partners/${p.img}`
+}));
