d47083e23a4f1c681628f7e7770d890e3557fd56
Alisa Korniienko <33879792+alisabond@users.noreply.github.com>
2025-10-28 15:00:30 +0200

    fix: close dropdown menus on scroll down in header (#778)
    * fix: close dropdown menus on scroll down in header
    
    * refactor: extract NavigationBar props into separate interface

--- app/shared/components/Header/RightActionsPanel/RightActionsPanel.tsx
+++ app/shared/components/Header/RightActionsPanel/RightActionsPanel.tsx
@@ -9,20 +9,22 @@ import useBreakpoints from '~/hooks/use-breakpoints/useBreakpoints';
 import AudioPlayer from '../AudioPlayer/AudioPlayer';
 import SupportButton from '../SupportButton/SupportButton';
 import { styles } from './RightActionsPanel.styles';
+import type { ScrollDirection } from '~/types/types/common.types';
 import { type SupportButtonData } from '~/types/types/header.type';
 
 interface SupportButtonDataProps {
   supportButtonData: SupportButtonData;
+  scrollDirection: ScrollDirection;
 }
 
-export default function RightActionsPanel({ supportButtonData }: Readonly<SupportButtonDataProps>) {
+export default function RightActionsPanel({ supportButtonData, scrollDirection }: Readonly<SupportButtonDataProps>) {
   const { isMobile } = useBreakpoints();
 
   return (
     <Box sx={!isMobile ? styles.backgroundContainer : undefined}>
       <Box sx={styles.controls(isMobile)}>
         <AudioPlayer />
-        <LanguageSwitcher variant="icon" />
+        <LanguageSwitcher variant="icon" scrollDirection={scrollDirection} />
       </Box>
       <SupportButton data={supportButtonData} isMobile={isMobile} />
     </Box>
