961bf2f03ebcc6e830f28d9a76d4a9867efa9c7e
Alisa Korniienko <alisabond78@gmail.com>
2025-10-28 22:20:10 +0200

    refactor: add data-testid attributes to ContactsInfo component

--- app/[lang]/contacts/ContactsInfo/ContactsInfo.tsx
+++ app/[lang]/contacts/ContactsInfo/ContactsInfo.tsx
@@ -22,36 +22,48 @@ export default function ContactsInfo({ title, formTitle, contacts, socialLinks }
   const t = useTranslations('contactsInfoPage');
 
   return (
-    <Box sx={styles.root}>
+    <Box sx={styles.root} data-testid="ContactsInfo">
       <Box sx={styles.wrapper}>
         <Box sx={styles.contactsInfoWrapper}>
           {title ? (
-            <Typography sx={styles.title}>{title}</Typography>
+            <Typography sx={styles.title} data-testid="ContactsInfo-title">
+              {title}
+            </Typography>
           ) : (
-            <Typography sx={styles.titleMain} variant="h2">
+            <Typography sx={styles.titleMain} variant="h2" data-testid="ContactsInfo-title">
               {t('contacts')}
             </Typography>
           )}
           <Box sx={styles.contactsDetails}>
-            <Box sx={styles.contacts}>
-              <Typography variant="subtitle1">{t('phoneNumber')}:</Typography>
-              <Link variant="customSemiBold18">{contacts.phone}</Link>
+            <Box sx={styles.contacts} data-testid="ContactsInfo-phoneSection">
+              <Typography variant="subtitle1" data-testid="ContactsInfo-phoneLabel">
+                {t('phoneNumber')}:
+              </Typography>
+              <Link variant="customSemiBold18" data-testid="ContactsInfo-phoneLink">
+                {contacts.phone}
+              </Link>
             </Box>
-            <Box sx={styles.contacts}>
-              <Typography variant="subtitle1">{t('email')}:</Typography>
-              <Link variant="customSemiBold18">{contacts.email}</Link>
+            <Box sx={styles.contacts} data-testid="ContactsInfo-emailSection">
+              <Typography variant="subtitle1" data-testid="ContactsInfo-emailLabel">
+                {t('email')}:
+              </Typography>
+              <Link variant="customSemiBold18" data-testid="ContactsInfo-emailLink">
+                {contacts.email}
+              </Link>
             </Box>
           </Box>
-          <Box sx={styles.socialMediaWrapper}>
-            <Typography variant="subtitle1">{t('socialMedia')}:</Typography>
+          <Box sx={styles.socialMediaWrapper} data-testid="ContactsInfo-socialMediaSection">
+            <Typography variant="subtitle1" data-testid="ContactsInfo-socialMediaLabel">
+              {t('socialMedia')}:
+            </Typography>
             <FooterSocialMedia media={socialLinks} />
           </Box>
         </Box>
-        <PaperComponent sx={styles.formWrapper}>
-          <Typography sx={styles.formTitle} variant="h5">
+        <PaperComponent sx={styles.formWrapper} data-testid="ContactsInfo-formWrapper">
+          <Typography sx={styles.formTitle} variant="h5" data-testid="ContactsInfo-formTitle">
             {formTitle ?? t('formTitle')}
           </Typography>
-          <Typography sx={styles.formSubtitle} variant="subtitle1">
+          <Typography sx={styles.formSubtitle} variant="subtitle1" data-testid="ContactsInfo-formSubtitle">
             {t('formSubtitle')}
           </Typography>
           <ContactForm onSubmit={() => {}} />
