c004cbcddd41910a09d874fabc9dbcddca1c8960
Alisa Korniienko <33879792+alisabond@users.noreply.github.com>
2025-10-31 13:53:02 +0200

    refactor: add data-testid attributes to Terms of Use page components (#802)
    * feat: add data-testid attributes to Terms of Use page components
    
    * refactor: use camelCase dataTestId prop instead of kebab-case

--- app/shared/components/blocks/terms-of-use/terms-content/button-content-block/ButtonContentBlock.tsx
+++ app/shared/components/blocks/terms-of-use/terms-content/button-content-block/ButtonContentBlock.tsx
@@ -16,15 +16,21 @@ type ButtonContentBlockProps = {
 };
 
 const ButtonContentBlock = ({ buttonText, buttonColor = 'primary', content, sx }: ButtonContentBlockProps) => (
-  <Box sx={styles.wrapper}>
+  <Box sx={styles.wrapper} data-testid="ButtonContentBlock">
     <Box sx={styles.buttonBox}>
-      <Button variant="contained" color={buttonColor} fullWidth sx={{ ...styles.button, ...sx }}>
+      <Button
+        variant="contained"
+        color={buttonColor}
+        fullWidth
+        sx={{ ...styles.button, ...sx }}
+        data-testid="ButtonContentBlock-button"
+      >
         {buttonText}
         <Svg Component={ArrowUpRight} alt="icon" color="#000" width="20px" height="20px" sx={styles.icon} />
       </Button>
     </Box>
 
-    <Box sx={styles.contentBox}>
+    <Box sx={styles.contentBox} data-testid="ButtonContentBlock-content">
       <ContentBlock description={content} containerSx={{ marginBottom: { xs: '32px', md: '40px' } }} />
     </Box>
   </Box>
