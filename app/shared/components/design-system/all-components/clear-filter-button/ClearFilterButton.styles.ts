e3bb7fc3f4662e3572ac166e976a407063ff11bf
Anastasia <105491259+qqwz0@users.noreply.github.com>
2025-11-02 12:52:18 +0200

    Fix(ui): unify clear filter button styles and extract into reusable component (#805)
    * fix(ui): unify clear filter button styles and extract into reusable component
    
    * refactor(ui): mark props as readonly and remove unnecessary 'use client'
    
    * refactor: refactor import

---
+++ app/shared/components/design-system/all-components/clear-filter-button/ClearFilterButton.styles.ts
@@ -0,0 +1,28 @@
+import { mainHexPallete, rgbaClearFilterButton } from '~/ds-components/theme/colors';
+
+export const styles = {
+  button: {
+    lineHeight: '140%',
+    width: '100%',
+    borderRadius: '8px',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    color: rgbaClearFilterButton.defaultTextColor,
+
+    '& svg path, & svg circle, & svg line, & svg rect': {
+      stroke: 'currentColor'
+    },
+
+    '&:hover': {
+      backgroundColor: mainHexPallete.red[50]
+    },
+
+    '&:focus-visible': {
+      color: mainHexPallete.red[700]
+    },
+
+    '&:active': {
+      color: mainHexPallete.red[700]
+    }
+  }
+};
