f1f54c03ac8572f291c3f21d0c1f76d05bbede9d
ruslansymonenko <symonenkors@gmail.com>
2025-11-03 20:48:34 +0200

    feat: create website block with volunteer donation info

---
+++ app/shared/components/blocks/volunteer-donation/VolunteerDonation.styles.ts
@@ -0,0 +1,82 @@
+export const styles = {
+  mainContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'subgrid',
+    gridColumn: '1 / -1',
+    mb: {
+      xs: '16px',
+      sm: '74px',
+      md: '100px'
+    }
+  },
+  title: {
+    marginBottom: {
+      xs: '24px',
+      md: '40px',
+      xxl: '40px'
+    }
+  },
+  contentWrapper: {
+    gridColumn: {
+      xs: '1 / 5',
+      sm: '4 / 8',
+      md: '6 / 11',
+      lg: '6 / 12'
+    },
+    mb: {
+      xs: '40px',
+      md: '60px',
+      xxl: '136px'
+    }
+  },
+  card: {
+    mb: {
+      xs: '16px',
+      md: '24px'
+    }
+  },
+  paypalLink: {
+    display: 'inline-block',
+    textDecoration: 'underline',
+    '&:hover': {
+      opacity: 0.8
+    }
+  },
+  img: {
+    gridColumn: {
+      xs: '2 / -1',
+      sm: '4 / -1',
+      md: '6 / 12'
+    },
+    gridRow: {
+      sm: '5 / 7'
+    },
+    mb: {
+      xs: '56px'
+    }
+  },
+  captionSx: {
+    mt: {
+      xs: '9px',
+      sm: '10px',
+      md: '14px'
+    },
+    maxWidth: {
+      xs: '224px',
+      sm: '457px',
+      md: '569px',
+      lg: '718px',
+      xl: '816px',
+      xxl: '1001px'
+    },
+    width: {
+      xs: '200px',
+      sm: '400px',
+      md: '496px',
+      lg: '645px',
+      xl: '744px',
+      xxl: '806px',
+      ultra: '1001px'
+    }
+  }
+};
---
+++ app/shared/components/blocks/volunteer-donation/VolunteerDonation.tsx
@@ -0,0 +1,67 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+import { ContactLink } from '~/components/contact-link/ContactLink';
+import ImageWithCaption from '~/components/image-with-caption/ImageWithCaption';
+import SectionTitle from '~/components/section-title/SectionTitle';
+
+import { styles } from './VolunteerDonation.styles';
+
+interface PaymentMethod {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  title: string;
+  paymentMethods: PaymentMethod[];
+  imageSrc: string;
+  caption?: string;
+}
+
+const VolunteerDonation: React.FC<Props> = ({ title, paymentMethods, imageSrc, caption }) => {
+  return (
+    <Box sx={styles.mainContainer}>
+      <SectionTitle
+        title={title}
+        sx={styles.title}
+        icon={false}
+        gridColumn={{ xs: '1 / -1', sm: '4 / -1', md: '6 / -1' }}
+      />
+
+      <Box sx={styles.contentWrapper}>
+        {paymentMethods.map((method, index) => (
+          <Box key={index} sx={styles.card}>
+            <ContactLink type="email" value={method.value} label={method.label} />
+          </Box>
+        ))}
+      </Box>
+
+      <ImageWithCaption
+        src={imageSrc}
+        alt={title}
+        caption={caption ?? ''}
+        captionSx={styles.captionSx}
+        align="right"
+        sizes={{
+          width: { xs: 224, sm: 457, md: 569, lg: 718, xl: 816, xxl: 979, ultra: 816 },
+          height: { xs: 138, sm: 292, md: 336, lg: 498, xl: 498, xxl: 498, ultra: 498 }
+        }}
+        border={{
+          sizes: {
+            width: { xs: 31, sm: 38, md: 57, lg: 80 },
+            height: { xs: 134, sm: 134, md: 204, lg: 360 }
+          },
+          top: { xs: 16, sm: 20, md: 36, lg: 38, xl: 40 },
+          left: { xs: 16, sm: 26, md: 41, lg: 40, xl: 40 }
+        }}
+        containerSx={styles.img}
+        imageSx={{
+          width: { xs: '200px', sm: '400px', md: '496px', lg: '645px', xl: '744px', xxl: '806px', ultra: '1001px' }
+        }}
+      />
+    </Box>
+  );
+};
+
+export default VolunteerDonation;
