ed8aafe9a00878cbe2b26504de2d2b0059646a8f
Taras <151529282+Taras-ep@users.noreply.github.com>
2025-11-04 01:31:26 +0200

    Chore/772/Defining identifiers for components on the "About Us" page (#809)
    * feat: add data-testid attributes to "About Us" page components (#772)
    
    * fix(section-title): use dataTestId prop instead of data-testid in WhatWeDo (#772)

--- app/shared/components/title-with-description/TitleWithDescription.tsx
+++ app/shared/components/title-with-description/TitleWithDescription.tsx
@@ -3,9 +3,9 @@ import { Box, Typography } from '@mui/material';
 import { styles } from './TitleWithDescription.styles';
 import { TitleWithDescriptionProps } from '~/types/types/titleWithDescriptionComponent';
 
-const TitleWithDescription = ({ variant, title, description }: TitleWithDescriptionProps) => {
+const TitleWithDescription = ({ variant, title, description, dataTestId }: TitleWithDescriptionProps) => {
   return (
-    <Box sx={styles.container(variant)}>
+    <Box sx={styles.container(variant)} data-testid={dataTestId}>
       <Typography sx={styles.blockTitle()}>{title}</Typography>
 
       {description && <Typography sx={styles.blockDescription()}>{description}</Typography>}
