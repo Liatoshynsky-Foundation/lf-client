d3c15714c78a9abe82cdefe849111ecd782f6d47
MaksFullJs <peliskom@gmail.com>
2025-11-06 12:25:14 +0200

    feat: create bullettextwithlinks component

---
+++ app/shared/components/design-system/all-components/bullet-text-with-links/BulletTextWithLinks.styles.ts
@@ -0,0 +1,64 @@
+export const styles = {
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: { xs: 'repeat(4,1fr)', sm: 'repeat(8, 1fr)', md: 'repeat(12, 1fr)' },
+    gridColumn: '1 / -1',
+    gridTemplateAreas: {
+      xs: ' "image"  "content" "buttons"',
+      sm: '"buttons content" '
+    },
+    columnGap: { xs: '20px', sm: '23px', md: '40px' },
+    rowGap: { xs: '13px', sm: '17px', md: '20px' },
+    alignItems: 'start',
+    height: 'fit-content'
+  },
+
+  buttonBox: {
+    gridArea: 'buttons',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 2,
+    justifySelf: 'end',
+    gridColumn: { xs: '1/ 6', sm: '1/4', md: '1 / 6' },
+    justifyContent: 'space-between',
+    width: '100%',
+    height: '100%'
+  },
+
+  button: {
+    alignSelf: { xs: 'start', sm: 'end' },
+    maxWidth: { xs: '200px', sm: '250px' }
+  },
+
+  icon: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+
+  contentBox: {
+    gridArea: 'content',
+    gridColumn: { xs: '1/ 6', sm: '1 / 9', md: '1 / 13' },
+    width: '100%'
+  },
+
+  image: {
+    gridArea: 'image',
+    position: 'relative',
+    width: {
+      xs: '22px',
+      md: '32px'
+    },
+    height: {
+      xs: '20px',
+      md: '30px'
+    }
+  },
+
+  buttonsBox: {
+    gridColumn: { xs: '1/ -1', sm: '4/ -1', md: '6/-1' },
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    gap: 2
+  }
+};
---
+++ app/shared/components/design-system/all-components/bullet-text-with-links/BulletTextWithLinks.tsx
@@ -0,0 +1,83 @@
+'use client';
+import { Box } from '@mui/material';
+import Image from 'next/image';
+
+import Button from '../button/Button';
+import ContentBlock from '../content-block/ContentBlock';
+import { styles } from './BulletTextWithLinks.styles';
+import { TipTapDoc } from '~/types/types/common.types';
+
+import { generateSizesAttribute } from '~/lib/utils/generateSizesAttribute';
+import ArrowUpRight from '~/public/icons/arrow-up-right.svg';
+import FacebookIcon from '~/public/icons/facebook.svg';
+import { Svg } from '~/shared/components/colored-svg/ColoredSvg';
+import useBreakpoints from '~/shared/hooks/use-breakpoints/useBreakpoints';
+
+type ButtonItem = {
+  shortText: string;
+  fullText: string;
+  link: string;
+};
+
+type RichContent = string | TipTapDoc;
+
+type IconButtonContentBlockProps = {
+  description?: RichContent;
+  buttons: ButtonItem[];
+  buttonText: string;
+};
+
+const imageSizes = {
+  width: {
+    xs: 22,
+    md: 32
+  },
+  height: {
+    xs: 20,
+    md: 30
+  }
+};
+
+export default function BulletTextWithLinks({ description, buttons, buttonText }: IconButtonContentBlockProps) {
+  const { isMobile } = useBreakpoints();
+  const sizesAttribute = generateSizesAttribute(imageSizes);
+
+  const imageBox = (
+    <Box sx={styles.image}>
+      <Image src="/icons/ellipse-gray.svg" alt="ellipse" fill sizes={sizesAttribute} />
+    </Box>
+  );
+
+  return (
+    <Box sx={styles.wrapper}>
+      {isMobile && imageBox}
+      <Box sx={styles.buttonBox}>
+        {!isMobile && imageBox}
+        <Button size="medium" variant="contained" sx={{ ...styles.button }}>
+          {buttonText}
+          <Svg Component={ArrowUpRight} alt="icon" color="#fff" width="20px" height="20px" sx={styles.icon} />
+        </Button>
+      </Box>
+
+      <Box sx={styles.contentBox}>
+        <ContentBlock description={description} textSx={{ gridColumn: { xs: '1/ -1', sm: '4/ -1', md: '6/-1' } }} />
+      </Box>
+      <Box sx={styles.buttonsBox}>
+        {buttons.map((button) => (
+          <Button key={button.shortText} link={button.link} externalLink={true} variant="outlined" size="medium">
+            {isMobile ? button.shortText : button.fullText}
+            <Svg
+              Component={FacebookIcon}
+              fill="none"
+              alt="icon"
+              color="#000"
+              width="20px"
+              height="20px"
+              sx={styles.icon}
+            />
+          </Button>
+        ))}
+      </Box>
+    </Box>
+  );
+}
