7af832098f66fe7f5ea09f5a2a1c0702b27cf493
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-11-06 11:51:15 +0200

    feat: add phone number validation regex and base length. Tests included (#782)

--- app/constants/errors.ts
+++ app/constants/errors.ts
@@ -5,6 +5,7 @@ import { createBlobErrors, createCredentialsErrors, createEnvErrors } from '~/li
 export const errors = {
   NAME_ERROR: `Name must be a string between ${lengths.NAME_MIN_LENGTH} and ${lengths.NAME_MAX_LENGTH} characters long.`,
   EMAIL_ERROR: 'Invalid email address.',
+  PHONE_NUMBER_ERROR: 'Invalid phone number',
   MESSAGE_ERROR: `Message must be a string between ${lengths.MESSAGE_MIN_LENGTH} and ${lengths.MESSAGE_MAX_LENGTH} characters.`,
   MISSING_MONGO_URL: '❌ mongoUrl is not defined or is empty',
   FAILED_TO_CONNECT_DB: '❌ Failed to connect to the database',
