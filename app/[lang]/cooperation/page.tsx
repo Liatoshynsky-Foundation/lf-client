8f1fa55f61c82dd9b423ee0da8ab2ab7f61fa8d0
kolibri753 <kolibri753@gmail.com>
2025-11-07 00:44:18 +0200

    fix(cooperation): remove absolute from OfferCollaboration and render outside MainLayout

--- app/[lang]/cooperation/page.tsx
+++ app/[lang]/cooperation/page.tsx
@@ -9,6 +9,7 @@ import { isProductionMode } from '~/utils/isProductionMode';
 import MainLayout from '~/layouts/main-layout/MainLayout';
 import CollaborationIntro from '~/shared/components/blocks/collaboration/collaboration-intro/CollaborationIntro';
 import { collaborationIntroPageData } from '~/shared/components/blocks/collaboration/collaboration-intro/CollaborationIntro.consts';
+import OfferCollaboration from '~/shared/components/blocks/collaboration/offer-collaboration/OfferCollaboration';
 
 export default async function CollaborationPage({ params }: Readonly<Language>) {
   const { lang } = await params;
@@ -18,13 +19,16 @@ export default async function CollaborationPage({ params }: Readonly<Language>) 
     return <UnderDevelopment />;
   }
   return (
-    <MainLayout withLines>
-      <CollaborationIntro
-        title={collaborationIntroPageData[lang].title}
-        subtitle={collaborationIntroPageData[lang].subtitle}
-        contentAbove={collaborationIntroPageData[lang].contentAbove}
-        content={collaborationIntroPageData[lang].content}
-      />
-    </MainLayout>
+    <>
+      <MainLayout withLines>
+        <CollaborationIntro
+          title={collaborationIntroPageData[lang].title}
+          subtitle={collaborationIntroPageData[lang].subtitle}
+          contentAbove={collaborationIntroPageData[lang].contentAbove}
+          content={collaborationIntroPageData[lang].content}
+        />
+      </MainLayout>
+      <OfferCollaboration />
+    </>
   );
 }
