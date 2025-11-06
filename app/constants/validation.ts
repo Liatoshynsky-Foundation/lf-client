45b46ec32eace146995bfd3720541f702cf2cb35
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-11-06 11:51:15 +0200

    feat: add phone number validation regex and base length. Tests included (#782)

--- app/constants/validation.ts
+++ app/constants/validation.ts
@@ -1,10 +1,13 @@
 export const lengths = {
   NAME_MIN_LENGTH: 2,
   NAME_MAX_LENGTH: 50,
+  PHONE_NUMBER_MAX_LENGTH: 16,
+  PHONE_NUMBER_MIN_LENGTH: 11,
   MESSAGE_MIN_LENGTH: 10,
   MESSAGE_MAX_LENGTH: 1000
 };
 
 export const regex = {
-  EMAIL_PATTERN: /^([a-z\d]+([._-][a-z\d]+)*)@([a-z\d]+([.-][a-z\d]+)*\.[a-z]{2,})$/i
+  EMAIL_PATTERN: /^([a-z\d]+([._-][a-z\d]+)*)@([a-z\d]+([.-][a-z\d]+)*\.[a-z]{2,})$/i,
+  PHONE_NUMBER_PATTERN: /^\+\d+$/
 };
