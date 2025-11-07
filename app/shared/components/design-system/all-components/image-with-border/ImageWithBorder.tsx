14d63caf1d60136cca197165bcbdeed61ca22b34
Ruslan Symonenko <99801898+ruslansymonenko@users.noreply.github.com>
2025-11-07 10:50:22 +0200

    chore: refactor image with border component to support flexible dimen… (#867)
    * chore: refactor image with border component to support flexible dimensions
    
    * fix: fix problem with component disappearancewith parent 0 height

--- app/shared/components/design-system/all-components/image-with-border/ImageWithBorder.tsx
+++ app/shared/components/design-system/all-components/image-with-border/ImageWithBorder.tsx
@@ -5,8 +5,8 @@ import { styles } from './ImageWithBorder.styles';
 
 interface ImageWithBorderProps {
   image: string;
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   borderWidth?: number;
   alt: string;
 }
@@ -18,10 +18,16 @@ export default function ImageWithBorder({
   borderWidth = 8,
   alt
 }: Readonly<ImageWithBorderProps>) {
+  const useFillMode = !width || !height;
+
   return (
     <Box sx={styles.container(width, height)}>
       <Box sx={styles.border(borderWidth, height)} />
-      <Image width={width} height={height} alt={alt} src={image} />
+      {useFillMode ? (
+        <Image alt={alt} src={image} fill style={{ objectFit: 'cover' }} />
+      ) : (
+        <Image width={width} height={height} alt={alt} src={image} />
+      )}
     </Box>
   );
 }
