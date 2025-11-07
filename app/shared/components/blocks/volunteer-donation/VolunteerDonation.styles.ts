8b9e2a99bc186c538b22de10d70a19b5b4673df5
ruslansymonenko <symonenkors@gmail.com>
2025-11-07 18:23:40 +0200

    chore: update component to fix code duplicate

--- app/shared/components/blocks/volunteer-donation/VolunteerDonation.styles.ts
+++ app/shared/components/blocks/volunteer-donation/VolunteerDonation.styles.ts
@@ -80,3 +80,21 @@ export const styles = {
     }
   }
 };
+
+export const imageSizes = {
+  sizes: {
+    width: { xs: 224, sm: 457, md: 569, lg: 718, xl: 816, xxl: 979, ultra: 816 },
+    height: { xs: 138, sm: 292, md: 336, lg: 498, xl: 498, xxl: 498, ultra: 498 }
+  },
+  border: {
+    sizes: {
+      width: { xs: 31, sm: 38, md: 57, lg: 80 },
+      height: { xs: 134, sm: 134, md: 204, lg: 360 }
+    },
+    top: { xs: 16, sm: 20, md: 36, lg: 38, xl: 40 },
+    left: { xs: 16, sm: 26, md: 41, lg: 40, xl: 40 }
+  },
+  imageSx: {
+    width: { xs: '200px', sm: '400px', md: '496px', lg: '645px', xl: '744px', xxl: '806px', ultra: '1001px' }
+  }
+};
