866c9a3374918c65643ae6ea7c5e735078075c0b
Ruslan Symonenko <99801898+ruslansymonenko@users.noreply.github.com>
2025-11-09 19:43:45 +0200

    Feature/821/volunteer donation block (#831)
    * feat: create website block with volunteer donation info
    
    * test: write test for component volunteer donation
    
    * fix: fixed items position for sm screens
    
    * chore: update component to fix code duplicate
    
    * chore: fix decor element color regarding to design
    
    * fix: sonarcloud issue fix
    
    * fix: apply review suggestions
    
    * fix: import fix
    
    * fix: change link type to phone
    
    ---------
    
    Co-authored-by: bandvov <48312647+bandvov@users.noreply.github.com>

---
+++ app/shared/components/blocks/volunteer-donation/VolunteerDonation.tsx
@@ -0,0 +1,55 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+import { ContactLink } from '~/components/contact-link/ContactLink';
+import ImageWithCaption from '~/components/image-with-caption/ImageWithCaption';
+import SectionTitle from '~/components/section-title/SectionTitle';
+
+import { imageSizes, styles } from './VolunteerDonation.styles';
+
+interface PaymentMethod {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  title: string;
+  paymentMethods: PaymentMethod[];
+  imageSrc: string;
+  caption?: string;
+}
+
+const VolunteerDonation: React.FC<Props> = ({ title, paymentMethods, imageSrc, caption }) => {
+  return (
+    <Box sx={styles.mainContainer}>
+      <SectionTitle
+        title={title}
+        sx={styles.title}
+        icon={false}
+        gridColumn={{ xs: '1 / -1', sm: '3 / -1', md: '6 / -1' }}
+      />
+
+      <Box sx={styles.contentWrapper}>
+        {paymentMethods.map((method, idx) => (
+          <Box key={`${method.label}-${idx}`} sx={styles.card}>
+            <ContactLink type="phone" value={method.value} label={method.label} />
+          </Box>
+        ))}
+      </Box>
+
+      <ImageWithCaption
+        src={imageSrc}
+        alt={title}
+        caption={caption ?? ''}
+        captionSx={styles.captionSx}
+        align="right"
+        sizes={imageSizes.sizes}
+        border={imageSizes.border}
+        containerSx={styles.img}
+        imageSx={imageSizes.imageSx}
+      />
+    </Box>
+  );
+};
+
+export default VolunteerDonation;
