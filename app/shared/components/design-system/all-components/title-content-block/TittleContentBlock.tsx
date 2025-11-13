1bcf6cb50f0c5f8f731c382a53524c3c771c645a
Pelyshko Maksym <peliskom@gmail.com>
2025-11-13 12:02:48 +0200

    feat: create titlecontentblock component (#898)
     Created TitleContentBlock component

---
+++ app/shared/components/design-system/all-components/title-content-block/TittleContentBlock.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material';
+
+import ContentBlock from '../content-block/ContentBlock';
+import { styles } from './TitleContentBlock.styles';
+import { TipTapDoc } from '~/types/types/common.types';
+
+type RichContent = string | TipTapDoc;
+
+type TitleContentBlockProps = {
+  title: string;
+  content: RichContent;
+  containerSx?: object;
+};
+
+export default function TitleContentBlock({ title, content, containerSx }: Readonly<TitleContentBlockProps>) {
+  return (
+    <Box data-testid="TitleContentBlock" sx={{ ...styles.mainContainer, ...containerSx }}>
+      <Typography data-testid="TitleContentBlock-title" sx={styles.title} variant="h5">
+        {title}
+      </Typography>
+      <Box sx={styles.contentBlock}>
+        <ContentBlock dataTestId="TitleContentBlock-contentBlock" textSx={styles.textStyle} description={content} />
+      </Box>
+    </Box>
+  );
+}
