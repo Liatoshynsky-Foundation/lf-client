f374bede843de353f48e01ccac12b048435090f9
stsv.t <167477637+stsvt@users.noreply.github.com>
2025-11-13 22:16:25 +0200

    feat: implement YearTabs component (#894)
    * feat: implement YearTabs component
    
    * fix: fix sonar issues
    
    * fix: fix remaining sonar issues

---
+++ app/shared/components/year-tabs/YearTabs.styles.ts
@@ -0,0 +1,25 @@
+export const styles = {
+  buttonGroup: {
+    position: 'fixed',
+    bottom: '5%',
+    left: '50%',
+    transform: 'translate(-50%)',
+    borderRadius: '48px',
+    p: '4px',
+    zIndex: 10,
+
+    '& [aria-label="indicator"]': {
+      height: 'calc(100% - 8px)',
+      top: 4
+    }
+  },
+  yearButton: {
+    border: 'none',
+    display: 'flex',
+    alignItems: 'center',
+
+    '&, &:hover': {
+      background: 'transparent'
+    }
+  }
+};
