933e7996074c03f74e8bfdd56250a808cdfe0793
Alisa Korniienko <alisabond78@gmail.com>
2025-11-18 16:10:53 +0200

    refactor(FundCard): extract base test ID to improve code quality

--- app/[lang]/archive/FundCard/FundCard.tsx
+++ app/[lang]/archive/FundCard/FundCard.tsx
@@ -12,22 +12,19 @@ export interface FundCardProps {
 }
 
 export default function FundCard({ id, number, title, dataTestId }: Readonly<FundCardProps>) {
+  const baseTestId = dataTestId || `FundCard-${id}`;
+
   return (
-    <Box
-      component="article"
-      sx={styles.card}
-      data-testid={dataTestId || `FundCard-${id}`}
-      aria-label={`${number}: ${title}`}
-    >
-      <Typography sx={styles.fundNumber} data-testid={`${dataTestId || `FundCard-${id}`}-number`}>
+    <Box component="article" sx={styles.card} data-testid={baseTestId} aria-label={`${number}: ${title}`}>
+      <Typography sx={styles.fundNumber} data-testid={`${baseTestId}-number`}>
         {number}
       </Typography>
 
-      <Typography className="fundTitle" sx={styles.fundTitle} data-testid={`${dataTestId || `FundCard-${id}`}-title`}>
+      <Typography className="fundTitle" sx={styles.fundTitle} data-testid={`${baseTestId}-title`}>
         {title}
       </Typography>
 
-      <Box className="fundLine" sx={styles.fundLine} data-testid={`${dataTestId || `FundCard-${id}`}-line`} />
+      <Box className="fundLine" sx={styles.fundLine} data-testid={`${baseTestId}-line`} />
     </Box>
   );
 }
