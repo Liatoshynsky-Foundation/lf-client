6f19973293c1fb3564d2bb5ae704f550926efaf9
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-11-24 21:16:57 +0200

    feat: add types (#660)

---
+++ app/types/page/biography.types.ts
@@ -0,0 +1,68 @@
+import type { TipTapDoc } from '../types/tiptap.types';
+
+type Locale = 'uk' | 'en';
+type LocalizedTipTapDoc = Record<Locale, TipTapDoc>;
+
+export enum ContentType {
+  ChronologyList = 'chronologyList',
+  ExcerptBlockItem = 'excerptBlockItem',
+  OnlyImageBlock = 'OnlyImageBlock',
+  FullWidthImage = 'fullWidthImage'
+}
+
+export enum ImagesSizes {
+  SmallHorizontal = 'smallHorizontal',
+  SmallVerticalThin = 'smallVerticalThin',
+  SmallVerticalWide = 'smallVerticalWide',
+  BigVertical = 'bigVertical',
+  BigHorizontal = 'bigHorizontal'
+}
+interface ChrolologyListItem {
+  description: LocalizedTipTapDoc;
+}
+interface BaseImage {
+  src: string;
+  alt: LocalizedTipTapDoc;
+  caption: LocalizedTipTapDoc;
+}
+
+interface AdvancedImage extends BaseImage {
+  size: ImagesSizes;
+}
+interface BaseQuoteBlock {
+  quoteText: LocalizedTipTapDoc;
+  sourceText: LocalizedTipTapDoc;
+}
+export interface BiographyContentItemBaseProps<T extends ContentType = ContentType> {
+  type: T;
+}
+export interface ChronologyList extends BiographyContentItemBaseProps<ContentType.ChronologyList> {
+  listItems: ChrolologyListItem[];
+  additionalImage?: AdvancedImage;
+}
+export interface ExcerptBlockItem extends BiographyContentItemBaseProps<ContentType.ExcerptBlockItem> {
+  quote: BaseQuoteBlock;
+}
+export interface OnlyImageBlock extends BiographyContentItemBaseProps<ContentType.OnlyImageBlock> {
+  mainImage: AdvancedImage;
+  additionalImage?: AdvancedImage;
+}
+export interface FullWidthImage extends BiographyContentItemBaseProps<ContentType.FullWidthImage> {
+  image: BaseImage;
+}
+
+export type BiographyContentItem = ChronologyList | ExcerptBlockItem | OnlyImageBlock | FullWidthImage;
+
+export interface BiographyContentBlock {
+  yearTitle: string | undefined;
+  items: BiographyContentItem[];
+}
+
+export interface BiographyContentProps {
+  data: BiographyContentBlock[];
+}
+
+export function tiptapToPlainText(value?: TipTapDoc): string {
+  if (!value) return '';
+  return value.content?.map((el) => el.content?.map((child) => child.text ?? '').join('')).join(' ') ?? '';
+}
