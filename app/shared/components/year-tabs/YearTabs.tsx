b4780bed0c563b86a2cec87189ad8082239deb12
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-11-25 03:14:07 +0200

    feat: delete harden data and add props (#660)

--- app/shared/components/year-tabs/YearTabs.tsx
+++ app/shared/components/year-tabs/YearTabs.tsx
@@ -2,7 +2,6 @@
 import { Box, Button } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 
-import { years } from '~/components/year-tabs/constants';
 import { styles } from '~/components/year-tabs/YearTabs.styles';
 import ButtonGroup from '~/ds-components/button-group/ButtonGroup';
 import useBreakpoints from '~/hooks/use-breakpoints/useBreakpoints';
@@ -10,7 +9,11 @@ import { useScrollDirection } from '~/hooks/use-scroll-direction/useScrollDirect
 
 const HEADER_OFFSET = 100;
 
-export default function YearTabs() {
+interface Data {
+  years: string[];
+}
+
+export default function YearTabs({ years }: Data) {
   const { isMobile } = useBreakpoints();
   const [year, setYear] = useState<string>(years[0]);
   const [isVisible, setIsVisible] = useState<boolean>(true);
