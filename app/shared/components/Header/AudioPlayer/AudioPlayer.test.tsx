888a59dbc8108b9fe2d9f0bbd91f51b4d618a077
Nataliia Kravchenko <nataly.krvch@gmail.com>
2025-12-08 15:06:12 +0200

    test: fix audioplayer test (#1026)
    * test: fix audioplayer test
    
    * style: clean up

--- app/shared/components/Header/AudioPlayer/AudioPlayer.test.tsx
+++ app/shared/components/Header/AudioPlayer/AudioPlayer.test.tsx
@@ -57,6 +57,7 @@ describe('AudioPlayer', () => {
 
     HTMLMediaElement.prototype.play = jest.fn().mockResolvedValue(undefined);
     HTMLMediaElement.prototype.pause = jest.fn();
+    HTMLMediaElement.prototype.load = jest.fn();
   });
 
   beforeEach(() => {
