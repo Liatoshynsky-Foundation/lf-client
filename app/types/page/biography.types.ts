4ccdf6febcd085aa6cfe4be7e429d6a471af88ef
Taras-ep <fvm.grinchisin.ti.2020.211.m@gmail.com>
2025-12-08 22:06:25 +0200

    feat: create schema for biography page; update existing types (#1029)

--- app/types/page/biography.types.ts
+++ app/types/page/biography.types.ts
@@ -1,8 +1,5 @@
 import type { TipTapDoc } from '../types/tiptap.types';
 
-type Locale = 'uk' | 'en';
-type LocalizedTipTapDoc = Record<Locale, TipTapDoc>;
-
 export enum ContentType {
   ChronologyList = 'chronologyList',
   ExcerptBlockItem = 'excerptBlockItem',
@@ -19,12 +16,12 @@ export enum ImagesSizes {
 }
 
 interface ChrolologyListItem {
-  description: LocalizedTipTapDoc;
+  description: TipTapDoc;
 }
 interface BaseImage {
   src: string;
-  alt: LocalizedTipTapDoc;
-  caption: LocalizedTipTapDoc;
+  alt: string;
+  caption: string | null;
 }
 
 interface AdvancedImage extends BaseImage {
@@ -32,8 +29,13 @@ interface AdvancedImage extends BaseImage {
   rectangleTopLeftCorner?: boolean;
 }
 interface BaseQuoteBlock {
-  quoteText: LocalizedTipTapDoc;
-  sourceText: LocalizedTipTapDoc;
+  text: string;
+  source: string;
+}
+
+interface BaseExpertBlock {
+  text: string;
+  source: string;
 }
 export interface BiographyContentItemBaseProps<T extends ContentType = ContentType> {
   type: T;
@@ -43,7 +45,7 @@ export interface ChronologyList extends BiographyContentItemBaseProps<ContentTyp
   additionalImage?: AdvancedImage;
 }
 export interface ExcerptBlockItem extends BiographyContentItemBaseProps<ContentType.ExcerptBlockItem> {
-  quote: BaseQuoteBlock;
+  quote: BaseExpertBlock;
 }
 export interface OnlyImageBlock extends BiographyContentItemBaseProps<ContentType.OnlyImageBlock> {
   mainImage: AdvancedImage;
@@ -56,7 +58,7 @@ export interface FullWidthImage extends BiographyContentItemBaseProps<ContentTyp
 export type BiographyContentItem = ChronologyList | ExcerptBlockItem | OnlyImageBlock | FullWidthImage;
 
 export interface BiographyContentBlock {
-  yearTitle: string | undefined;
+  yearTitle: string | null;
   items: BiographyContentItem[];
 }
 
@@ -65,27 +67,22 @@ export interface BiographyContentProps {
 }
 
 interface HeroImageCaption {
-  mainText: LocalizedTipTapDoc;
-  yearText: LocalizedTipTapDoc;
+  mainText: string;
+  yearText: string;
 }
 interface HeroImage {
   src: string;
-  alt: LocalizedTipTapDoc;
+  alt: string;
   caption: HeroImageCaption;
 }
 export interface BiographyHeroData {
   quote: BaseQuoteBlock;
   image: HeroImage;
-  biographyText: LocalizedTipTapDoc;
-  noteText: LocalizedTipTapDoc;
+  biographyText: TipTapDoc;
+  noteText: string;
 }
 
 export interface BiographyHeroProps {
   data: BiographyHeroData;
   years: string[];
-}
-
-export function tiptapToPlainText(value?: TipTapDoc): string {
-  if (!value) return '';
-  return value.content?.map((el) => el.content?.map((child) => child.text ?? '').join('')).join(' ') ?? '';
 }
