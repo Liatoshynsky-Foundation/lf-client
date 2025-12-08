d44e6b28a9cf69f29ced23bd3ccfa8299936b964
MaksFullJs <peliskom@gmail.com>
2025-12-08 16:20:21 +0200

    fix: fix usepagination test2

--- app/shared/hooks/use-pagination/usePagination.test.ts
+++ app/shared/hooks/use-pagination/usePagination.test.ts
@@ -57,7 +57,7 @@ describe('usePagination', () => {
 
     expect(result.current.currentPage).toBe(3);
     expect(result.current.visiblePages).toBe(1);
-    expect(result.current.paginatedData).toEqual(mockData.slice(10, 20));
+    expect(result.current.paginatedData).toEqual(mockData.slice(20, 25));
     expect(result.current.hasMore).toBe(true);
   });
 
