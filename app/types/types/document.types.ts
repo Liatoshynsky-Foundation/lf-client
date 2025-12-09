c21b10a8405e3f25240aff17f30d74821e388277
MaksFullJs <peliskom@gmail.com>
2025-12-09 21:47:12 +0200

    refactor: refactor documentstable to use real data

---
+++ app/types/types/document.types.ts
@@ -0,0 +1,9 @@
+export type DocumentRecord = {
+  id: string;
+  cipher: string;
+  name: string;
+  dates: string;
+  sheets: number | null;
+  contentDescription: string;
+  pdfUrl: string | null;
+};
