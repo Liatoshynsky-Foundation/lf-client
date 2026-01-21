3f2aa81f9abb42ab74e27342f3621aad2bfc9e66
MaksFullJs <peliskom@gmail.com>
2026-01-21 20:53:51 +0200

    fix: add spacing between header and content on contact page

--- app/[lang]/contacts/ContactsInfo/ContactsInfo.styles.ts
+++ app/[lang]/contacts/ContactsInfo/ContactsInfo.styles.ts
@@ -2,6 +2,8 @@ import { mainHexPallete } from '~/shared/components/design-system/all-components
 
 export const styles = {
   root: {
+    mt: { xs: '35px', sm: '70px' },
+    mb: { xs: '55px', sm: '100px' },
     gridColumn: '1 / -1',
     maxWidth: '1728px',
     width: '100%',
