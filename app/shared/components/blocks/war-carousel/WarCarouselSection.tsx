ea13761b7bb02ed6d176b734f6b8578cb44efb2b
unknown <dashafedoreeva5@gmail.com>
2026-02-27 19:45:12 +0200

    fix(ui): remove placeholder captions from War in Ukraine slider
    - remove hardcodedо фото' placeholder text from slider compone
    
    Closes #1157

--- app/shared/components/blocks/war-carousel/WarCarouselSection.tsx
+++ app/shared/components/blocks/war-carousel/WarCarouselSection.tsx
@@ -9,19 +9,19 @@ const images = [
     id: 1,
     src: '/images/war-in-ukraine-page/carousel/photo-1.png',
     alt: 'Carousel Image 1',
-    description: 'Підпис до фото 1'
+    description: ''
   },
   {
     id: 2,
     src: '/images/war-in-ukraine-page/carousel/photo-2.png',
     alt: 'Carousel Image 2',
-    description: 'Підпис до фото 2'
+    description: ''
   },
   {
     id: 3,
     src: '/images/war-in-ukraine-page/carousel/photo-3.png',
     alt: 'Carousel Image 3',
-    description: 'Підпис до фото 3'
+    description: ''
   }
 ];
 
