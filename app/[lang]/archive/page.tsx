8a505746a1a08cf522c56e8f382e4a7c1d67ac35
Denis <152603666+DenisGordProgrammer@users.noreply.github.com>
2026-03-03 13:39:00 +0200

    fix: implement localization for funds list in Archive page (#1206)
    * fix: translate funds list based on locale
    
    * fix: define FundData type for better type safety in funds translation
    
    * fix test
    
    ---------
    
    Co-authored-by: Denis Gordeev <povar572349@gmail.com>

--- app/[lang]/archive/page.tsx
+++ app/[lang]/archive/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Box, CircularProgress } from '@mui/material';
+import { useLocale } from 'next-intl';
 import { useEffect, useState } from 'react';
 
 import useBreakpoints from '~/hooks/use-breakpoints/useBreakpoints';
@@ -38,6 +39,7 @@ const getColumnPaddingTop = (columnNum: number) => {
 export default function Archive() {
   const { isMobile, isTablet, isLaptop, isDesktop } = useBreakpoints();
   const [error, setError] = useState<string | null>(null);
+  const locale = useLocale();
 
   const [funds, setFunds] = useState<FundDTO[]>([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -46,7 +48,7 @@ export default function Archive() {
   useEffect(() => {
     const loadFunds = async () => {
       try {
-        const response = await fetch('/api/funds');
+        const response = await fetch(`/api/funds?lang=${locale}`);
         if (!response.ok) {
           throw new Error('Failed to fetch funds');
         }
@@ -61,7 +63,7 @@ export default function Archive() {
     };
 
     loadFunds();
-  }, []);
+  }, [locale]);
 
   if (error) {
     throw new Error(error);
