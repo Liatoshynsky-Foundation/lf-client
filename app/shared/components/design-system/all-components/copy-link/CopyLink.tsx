7da483e0b392563c10ae092f1bbda48d98a61a0c
Pelyshko Maksym <peliskom@gmail.com>
2026-03-05 14:50:49 +0200

    fix: fix fontSize for span elements on war-in-ukraine page (#1235)

--- app/shared/components/design-system/all-components/copy-link/CopyLink.tsx
+++ app/shared/components/design-system/all-components/copy-link/CopyLink.tsx
@@ -110,7 +110,7 @@ function CopyLink({
       data-testid="CopyLink"
       sx={[styles.wrapper, copyLinkStyles, ...sxToArray(sx)]}
     >
-      <Typography component="span" variant={variant} data-testid="CopyLink-text">
+      <Typography sx={{ ...sx }} component="span" variant={variant} data-testid="CopyLink-text">
         {value}
       </Typography>
       <TooltipCustom title={finalHint} isOpen={isCopied} showArrow>
