c995df17073aeff0b3c702872f0087a748b006d1
Jevgan <skinnycoding@gmail.com>
2026-05-21 13:13:24 +0300

    chore: make TitleWithDescription take TipTap values

--- app/shared/components/title-with-description/TitleWithDescription.tsx
+++ app/shared/components/title-with-description/TitleWithDescription.tsx
@@ -1,14 +1,50 @@
 import { Box, Typography } from '@mui/material';
+import React from 'react';
 
+import { renderData } from '../tip-tap-content/nodes';
+import TipTapContent from '../tip-tap-content/TipTapContent';
 import { styles } from './TitleWithDescription.styles';
+import { TipTapNodeTypes } from '~/types/enums/common.enums';
 import { TitleWithDescriptionProps } from '~/types/types/titleWithDescriptionComponent';
 
 const TitleWithDescription = ({ variant, title, description, dataTestId }: TitleWithDescriptionProps) => {
   return (
     <Box sx={styles.container(variant)} data-testid={dataTestId}>
-      <Typography sx={styles.blockTitle()}>{title}</Typography>
+      {typeof title === 'string' ? (
+        <>
+          <Typography sx={styles.blockTitle()}>{title}</Typography>
+          {description && (
+            <TipTapContent
+              data={renderData(description)}
+              nodeRenderers={{
+                [TipTapNodeTypes.paragraph]: (children) => (
+                  <Typography sx={styles.blockDescription()}>{children}</Typography>
+                )
+              }}
+            />
+          )}
+        </>
+      ) : (
+        <>
+          <TipTapContent
+            data={title}
+            nodeRenderers={{
+              [TipTapNodeTypes.paragraph]: (children) => <Typography sx={styles.blockTitle()}>{children}</Typography>
+            }}
+          />
 
-      {description && <Typography sx={styles.blockDescription()}>{description}</Typography>}
+          {description && (
+            <TipTapContent
+              data={renderData(description)}
+              nodeRenderers={{
+                [TipTapNodeTypes.paragraph]: (children) => (
+                  <Typography sx={styles.blockDescription()}>{children}</Typography>
+                )
+              }}
+            />
+          )}
+        </>
+      )}
     </Box>
   );
 };
