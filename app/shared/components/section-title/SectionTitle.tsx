8f266c7aba7376461d645728b93060a083665e55
Jevgan <skinnycoding@gmail.com>
2026-05-21 13:12:57 +0300

    chore: make SectionTitle take TipTap values

--- app/shared/components/section-title/SectionTitle.tsx
+++ app/shared/components/section-title/SectionTitle.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import { Box, SxProps, Theme, Typography } from '@mui/material';
 import Image from 'next/image';
-import { useLocale } from 'next-intl';
+import { Locale, useLocale } from 'next-intl';
+import React from 'react';
 
+import TipTapContent from '../tip-tap-content/TipTapContent';
 import { imageSizes, styles } from './SectionTitle.styles';
+import { TipTapNodeTypes } from '~/types/enums/common.enums';
+import { TipTapDoc } from '~/types/types/tiptap.types';
 import { sxToArray } from '~/utils/sxToArray';
 
 import { generateSizesAttribute } from '~/lib/utils/generateSizesAttribute';
+import { getPlainString, isTipTapDoc } from '~/lib/utils/tiptapHelpers';
+
+type LocalizedString = Record<Locale, string>;
 
 interface SectionTitleProps {
   icon?: boolean;
   mb?: number | string;
   gridColumn?: object;
-  title: { uk: string; en: string } | string;
+  title: string | LocalizedString | TipTapDoc;
   sx?: SxProps<Theme>;
   dataTestId?: string;
 }
@@ -27,13 +36,31 @@ const SectionTitle: React.FC<SectionTitleProps> = ({ icon = true, mb, title, gri
           <Image src="/icons/ellipse.svg" alt="ellipse" fill sizes={sizesAttribute} />
         </Box>
       )}
-      <Typography
-        sx={styles.title(gridColumn)}
-        component="h2"
-        {...(dataTestId ? { 'data-testid': `${dataTestId}-title` } : {})}
-      >
-        {typeof title === 'string' ? title : title[locale]}
-      </Typography>
+
+      {isTipTapDoc(title) ? (
+        <TipTapContent
+          data={title as unknown as TipTapDoc}
+          nodeRenderers={{
+            [TipTapNodeTypes.paragraph]: (children) => (
+              <Typography
+                sx={styles.title(gridColumn)}
+                component="h2"
+                {...(dataTestId ? { 'data-testid': `${dataTestId}-title` } : {})}
+              >
+                {children}
+              </Typography>
+            )
+          }}
+        />
+      ) : (
+        <Typography
+          sx={styles.title(gridColumn)}
+          component="h2"
+          {...(dataTestId ? { 'data-testid': `${dataTestId}-title` } : {})}
+        >
+          {getPlainString(title, locale)}
+        </Typography>
+      )}
     </Box>
   );
 };
