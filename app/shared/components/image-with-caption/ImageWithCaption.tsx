b6e438ec67175c4e75542a0fc019594d8a831983
Jevgan <skinnycoding@gmail.com>
2026-05-21 13:12:34 +0300

    chore: make ImageWithCaption take TipTapValues

--- app/shared/components/image-with-caption/ImageWithCaption.tsx
+++ app/shared/components/image-with-caption/ImageWithCaption.tsx
@@ -1,11 +1,20 @@
+'use client';
+
 import { Box, BoxProps, Breakpoint, Typography, TypographyProps } from '@mui/material';
+import { JSONContent } from '@tiptap/react';
 import Image from 'next/image';
-import { Locale, useLocale } from 'next-intl';
+import { useLocale } from 'next-intl';
+import React from 'react';
+
+import TipTapContent from '~/components/tip-tap-content/TipTapContent';
 
 import { styles } from './ImageWithCaption.styles';
-import { ElementSizes } from '~/types/types/common.types';
+import { TipTapNodeTypes } from '~/types/enums/common.enums';
+import { ElementSizes, LocalizedString } from '~/types/types/common.types';
+import { TipTapDoc } from '~/types/types/tiptap.types';
 
 import { generateSizesAttribute } from '~/lib/utils/generateSizesAttribute';
+import { extractTextFromTipTap, getPlainString, isTipTapDoc } from '~/lib/utils/tiptapHelpers';
 
 export interface BorderProps {
   sizes: ElementSizes;
@@ -14,12 +23,10 @@ export interface BorderProps {
   color?: string;
 }
 
-type LocalizedString = Record<Locale, string>;
-
 interface ImageWithCaptionProps {
   src: string;
-  alt: string | LocalizedString;
-  caption: string | LocalizedString | null;
+  alt: string | LocalizedString | JSONContent;
+  caption: string | LocalizedString | JSONContent | null;
   sizes: ElementSizes;
   border?: BorderProps;
   align?: 'left' | 'right';
@@ -45,6 +52,11 @@ const ImageWithCaption: React.FC<ImageWithCaptionProps> = ({
 }) => {
   const sizesAttribute = generateSizesAttribute(sizes);
   const locale = useLocale();
+
+  const resolvedAltText = isTipTapDoc(alt)
+    ? extractTextFromTipTap(alt, locale)
+    : getPlainString(alt as string | LocalizedString, locale);
+
   return (
     <Box sx={{ ...styles.container, ...containerSx } as BoxProps['sx']} data-testid={dataTestId}>
       <Box sx={{ ...styles.imageContainer(sizes), ...imageSx } as BoxProps['sx']}>
@@ -53,18 +65,34 @@ const ImageWithCaption: React.FC<ImageWithCaptionProps> = ({
           style={styles.image as React.CSSProperties}
           src={src}
           fill
-          alt={typeof alt === 'string' ? alt : alt[locale]}
+          alt={resolvedAltText}
           sizes={sizesAttribute}
         />
       </Box>
-      {caption && (
-        <Typography
-          className={captionClassName}
-          sx={[styles.caption(sizes, align), captionSx].flat() as TypographyProps['sx']}
-        >
-          {typeof caption === 'string' ? caption : caption[locale]}
-        </Typography>
-      )}
+
+      {caption &&
+        (isTipTapDoc(caption) ? (
+          <TipTapContent
+            data={caption as unknown as TipTapDoc}
+            nodeRenderers={{
+              [TipTapNodeTypes.paragraph]: (children) => (
+                <Typography
+                  className={captionClassName}
+                  sx={[styles.caption(sizes, align), captionSx].flat() as TypographyProps['sx']}
+                >
+                  {children}
+                </Typography>
+              )
+            }}
+          />
+        ) : (
+          <Typography
+            className={captionClassName}
+            sx={[styles.caption(sizes, align), captionSx].flat() as TypographyProps['sx']}
+          >
+            {getPlainString(caption as string | LocalizedString)}
+          </Typography>
+        ))}
     </Box>
   );
 };
