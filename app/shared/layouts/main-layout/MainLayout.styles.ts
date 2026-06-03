c0fef659d02da7bcf8c90032881764a6317d537d
Anna Komar <126970705+annak413@users.noreply.github.com>
2026-06-03 11:54:58 +0300

    refactor(ui): separate grid layout from decorative vertical lines (#1377)

--- app/shared/layouts/main-layout/MainLayout.styles.ts
+++ app/shared/layouts/main-layout/MainLayout.styles.ts
@@ -1,28 +1,35 @@
-export const styles = {
-  width: '100%',
-  maxWidth: '1728px',
-  minHeight: 'inherit',
-  display: 'grid',
-  gridTemplateColumns: {
-    xs: 'repeat(4, 1fr)',
-    sm: 'repeat(8, 1fr)',
-    md: 'repeat(12, 1fr)'
+import type { SxProps, Theme } from '@mui/material/styles';
+
+export const styles: Record<string, SxProps<Theme>> = {
+  wrapper: {
+    width: '100%',
+    maxWidth: '1728px',
+    minHeight: 'inherit',
+    m: '0 auto',
+    position: 'relative',
+    pt: {
+      xs: '70px',
+      sm: '82px',
+      md: '100px',
+      lg: '92px'
+    },
+    px: {
+      xs: '24px',
+      sm: '56px',
+      md: '72px'
+    }
   },
-  columnGap: {
-    xs: '16px',
-    sm: '24px',
-    md: '40px'
-  },
-  m: '0 auto',
-  pt: {
-    xs: '70px',
-    sm: '82px',
-    md: '100px',
-    lg: '92px'
-  },
-  px: {
-    xs: '24px',
-    sm: '56px',
-    md: '72px'
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: {
+      xs: 'repeat(4, 1fr)',
+      sm: 'repeat(8, 1fr)',
+      md: 'repeat(12, 1fr)'
+    },
+    columnGap: {
+      xs: '16px',
+      sm: '24px',
+      md: '40px'
+    }
   }
 };
