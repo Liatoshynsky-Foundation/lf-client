c39f48228336a8b8cdf0480f20041f6bd13bde53
varenichek <vasandra22@gmail.com>
2026-06-18 14:02:16 +0300

    refactor(fix): sonarQube fix

--- app/shared/components/events-section/EventSection.styles.ts
+++ app/shared/components/events-section/EventSection.styles.ts
@@ -1,3 +1,5 @@
+import { navButtonStyles } from '../content-slider/ContentSlider.styles';
+
 export const styles = {
   sectionContainer: {
     display: 'grid',
@@ -171,50 +173,7 @@ export const styles = {
     pr: '20px',
     textTransform: 'none'
   },
-  navButton: {
-    position: 'static !important',
-    width: '64px !important',
-    height: '48px !important',
-    borderRadius: '100px',
-    border: '2px solid',
-    borderColor: 'black',
-    backgroundColor: 'transparent',
-    cursor: 'pointer',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transition: 'all 0.3s ease-in-out',
-    '&:after': {
-      content: '""'
-    },
-    '&:before': {
-      content: '""'
-    },
-    '& .swiper-navigation-icon': {
-      display: 'none'
-    },
-    '& img': {
-      filter: 'brightness(0)',
-      transition: 'filter 0.3s ease-in-out'
-    },
-    '&:hover': {
-      backgroundColor: 'black',
-      '& img': {
-        filter: 'brightness(0) invert(1)'
-      }
-    },
-    '&.swiper-button-disabled': {
-      opacity: 0.3,
-      cursor: 'not-allowed',
-      '&:hover': {
-        backgroundColor: 'transparent',
-        '& svg': {
-          filter: 'none'
-        }
-      }
-    },
-    margin: '0 !important'
-  },
+  navButton: navButtonStyles,
 
   regButton: {
     textTransform: 'none',
