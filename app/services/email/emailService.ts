3754c8d6e303e32734004c25b02c9bd7cda12478
tl <andrewzhubinskiy21@gmail.com>
2026-07-08 15:17:48 +0300

    fix: set SMTP transport security to true explicitly

--- app/services/email/emailService.ts
+++ app/services/email/emailService.ts
@@ -21,7 +21,7 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 465,
-      secure: process.env.SMTP_SECURE || true,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
