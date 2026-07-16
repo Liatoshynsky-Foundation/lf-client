eb0729c0d09514242583e6d24f3b2e8475edf07e
Anatskyi Vasyl <vasandra22@gmail.com>
2026-07-16 15:16:38 +0300

    fix: correct sx prop flattening in Typography (#1502)

--- app/shared/components/design-system/all-components/content-block/ContentBlock.tsx
+++ app/shared/components/design-system/all-components/content-block/ContentBlock.tsx
@@ -28,7 +28,7 @@ type ContentBlockProps = Readonly<{
 
 const createParagraph = (paragraphSx?: SxProps<Theme>) => {
   const ParagraphRenderer = (children: React.ReactNode) => (
-    <Typography sx={[[{ display: 'block' }, paragraphSx]].flat() as SxProps<Theme>}>{children}</Typography>
+    <Typography sx={[{ display: 'block' }, paragraphSx].flat() as SxProps<Theme>}>{children}</Typography>
   );
   ParagraphRenderer.displayName = 'ParagraphRenderer';
   return ParagraphRenderer;
